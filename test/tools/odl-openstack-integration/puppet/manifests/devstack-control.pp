fdfa12e1dd2747a64d72263a4eec99fda549123a
Dave Tucker <dave@dtucker.co.uk>
2014-07-24 15:03:34 +0100

    Initial Commit of ODL OpenStack Integration
    - Add Vagrantfile for Devstack VMs
    - Add Puppet manifests for provisioning the VMs
    
    Change-Id: I099dd25feb2bdeafee6ef360b346cbe10ab9ac00
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

---
+++ test/tools/odl-openstack-integration/puppet/manifests/devstack-control.pp
@@ -0,0 +1,17 @@
+vcsrepo { '/home/vagrant/devstack':
+    ensure   => present,
+    provider => git,
+    user     => 'vagrant',
+    source   => 'https://github.com/openstack-dev/devstack.git',
+    revision => $devstack_branch,
+    before   => File['/home/vagrant/devstack/local.conf']
+}
+
+$hosts = hiera('hosts')
+
+file { '/home/vagrant/devstack/local.conf':
+    ensure  => present,
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    content => template('/vagrant/puppet/templates/control.local.conf.erb')
+}
