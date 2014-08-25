df067a117fc908960464d779cbbadc36c6a293ca
Jamo Luhrsen <james.luhrsen@hp.com>
2014-08-25 16:06:16 -0700

    o introducing FlowLib robot/python library.  Gives ability to dynamically build flows, as opposed to pulling from a static file.  As it is improved and used, it should allow an easier way to build new/different test cases
    o TTL suite now uses the FlowLib keywords to do it's work.  Test cases are
    the same, and results should be the same.
    
    o TTL suite has better documentation and tagging
    
    Change-Id: I45c8f0421c586080b73197b139b87baf6c49338a
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>

---
+++ test/csit/libraries/FlowLib.py
@@ -0,0 +1,81 @@
+"""
+Library for dynamic flow construction.
+Authors: james.luhrsen@hp.com
+Updated: 2014-08-29
+"""
+'''
+xmltodict and json libs not needed at this point, but may be useful in
+the future.
+'''
+##import xmltodict
+##import json
+import string
+import robot
+from robot.libraries.BuiltIn import BuiltIn
+
+##bare bones xml for building a flow
+xml_skeleton = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +      \
+               '<flow xmlns="urn:opendaylight:flow:inventory">'         +      \
+                    '<instructions></instructions>'                     +      \
+                    '<match></match>'                                   +      \
+               '</flow>'
+
+class Flow:
+    '''
+    Flow class for creating and interacting with OpenFlow flows
+    '''
+
+    strict = "false"
+    instruction_xmls = ""
+    match_xmls = ""
+    cookie = 0
+    cookie_mask = 0
+    table_id = 0
+    id = 1
+    hard_timeout = 60
+    idle_timeout = 30
+    flow_name = "No Name"
+    priority = 0
+    barrier = "false"
+
+    xml = xml_skeleton
+
+    json = ""
+
+    def set_field(self, field, value):
+        '''
+           allows for generically setting any attribute in this
+           class based on the 'field' passed in.  In the future,
+           adding a new attribute only requires that single line
+           addition.  no need for additional setter.
+        '''
+        setattr(self, field, value)
+
+def Make_Flow_Object():
+    '''
+        Robot Keyword to create and return an instance of the Flow
+        class.
+    '''
+    flow = Flow()
+    return flow
+
+def Set_Flow_Field(flow, field, value):
+    '''
+        Robot Keyword to allow the modification (setting) of the 
+        flow object attributes
+    '''
+    flow.set_field(field,value)
+    return flow
+
+
+#def Convert_Flow_XML_To_Json(flow):
+#    '''
+#       There may be a need in the future to use json to push
+#       flows, as opposed to xml format that is prevalent in 
+#       test code at this point.  This function will give a 
+#       conversion, but unsure if it's proper.  Also, unsure
+#       if the xmltodict library is viable in the CSIT environment
+#    '''
+#    flowXmlDict = xmltodict.parse(flow.xml)
+#    flow.json = json.dumps(flowXmlDict)
+#    return flow
