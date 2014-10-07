36ec25740d4fbb77eebd7768afa0844b8e31bd35
Jamo Luhrsen <james.luhrsen@hp.com>
2014-10-07 13:33:14 -0700

    Switch Abstraction POC.
    - switch user made dynamic and defaults to ${MININET_USER}
    - added ssh_key functionality for switches that use ssh for mgmt_protocol
    - moved switch classes to a subdir in libraries for tidiness
    - made Ovs default and SWITCH_IP == ${MININET} so that no changes
      need to be made to jenkins job
    
    Change-Id: Ie563250ffd0b52c7c038f22fefde49f09ace209b
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>
    
    using @property for dynamic switch class attribute strings
    
    moved some attributes to properties so that the values of the
    properties could change dynamically as other class attributes
    are changed (e.g. when you get a flow object you need to update
    the controller_ip, and because of that a lot of the strings
    (for configs, etc) need to be updated in real time)
    
    Change-Id: I82cbafda72927a56b73d2196d9694d426e855b8c
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>
    
    moved generic keywords to new SwitchUtils library
    
    also:
    -  ${SWITCH} and ${CONTROLLER_IP} need to be passed as cmdLine
       args regexp will now match verifications, so long strings
       can be condensed in object definition (if desired)
    
    Change-Id: I87c7357c16cf971eb7c4ece0014dbb6228b309a6
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>
    
    renamed file
    
    Change-Id: Ibd114274a1c402cb40b9c3d0ce7cf4530f9b6aa2
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>
    
    OpenFlow validation enhancements
    
    Actions Suite ported to use new switch abstraction.
    
    Change-Id: I2f1f735c73bdfd7a1b3e58a6a8647f8bfeabf586
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>
    
    ${switch} object needed for read, write, execute wrappers
    
    Change-Id: I7dce87eca1c3040c65716fc5585b9a2eb724a86b
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>

---
+++ test/csit/libraries/SwitchClasses/ProVision.py
@@ -0,0 +1,166 @@
+"""
+Provision 3800 Object Definition
+Authors: james.luhrsen@hp.com
+Created: 2014-10-02
+"""
+import string
+import robot
+import re
+from robot.libraries.BuiltIn import BuiltIn
+from BaseSwitch import *
+
+class ProVision(BaseSwitch):
+    '''
+    ProVision Super Class
+    '''
+
+    make = 'provision'
+    model = ''
+
+    mgmt_protocol = 'telnet'
+    mgmt_ip = ''
+    mgmt_port = ''
+    mgmt_prompt = model + '.*#'
+
+    initialization_type = 'reboot'
+
+    of_instance_id = '21'
+
+    @property
+    def connection_configs(self):
+        return \
+            ['\rend \
+             \rconfig \
+             \rconsole local-terminal none \
+             \rno page \
+             \rend\r']
+
+    @property
+    def initialization_cmds(self):
+        return \
+            ['\rend\rboot system flash primary config odl_test_startup_config\r', \
+             'y', \
+             'n']
+
+    @property
+    def cleanup_cmds(self):
+        return \
+            ['end', \
+             'config', \
+             'no openflow\r \
+             y']
+
+    @property
+    def base_openflow_config(self):
+        return \
+            'end', \
+            'config', \
+            'openflow', \
+            'controller-id ' + self.of_instance_id + ' ip ' + self.of_controller_ip + \
+                            ' controller-interface oobm', \
+            'instance ' + self.of_instance_id, \
+            'member vlan 10', \
+            'controller-id ' + self.of_instance_id + ' ', \
+            'version 1.3', \
+            'enable', \
+            'openflow enable', \
+            'end'
+
+    @property
+    def openflow_enable_config(self):
+        return \
+            ['end', \
+             'config', \
+             'openflow enable', \
+             'end']
+
+    @property
+    def openflow_validation_cmd(self):
+        return \
+            'show openflow'
+
+    @property
+    def openflow_enable_validations(self):
+        return \
+            ['OpenFlow +: Enabled', \
+             self.of_instance_id + ' +Up +2 +1 +1.3']
+
+    @property
+    def openflow_disable_config(self):
+        return \
+            ['end', \
+             'config', \
+             'openflow disable', \
+             'end']
+
+    @property
+    def openflow_disable_validations(self):
+        return \
+            ['OpenFlow +: Disabled', \
+             self.of_instance_id + ' +Down +0 +0 +1.3']
+
+    @property
+    def dump_all_flows(self):
+        return \
+            'show openflow instance ' + self.of_instance_id + ' flows'
+
+    @property
+    def flow_validations(self):
+        return \
+            ['(?ms)Flow Table ID : 0.*Flow Table ID : 100.*' + \
+             'Source Protocol Address : ' + self.ip_src + '.*' + \
+             'Target Protocol Address : ' + self.ip_dst + '.*' + \
+             'Flow Table ID : ' + self.table_id + '.*' + self.action, \
+             'Source MAC    : ' + self.src_mac, \
+             'Destination MAC  : ' + self.dst_mac]
+
+    def create_flow_match_elements(self, flow_xml):
+        super(ProVision, self).create_flow_match_elements(flow_xml)
+        self.src_mac = self.format_mac_with_no_hyphens_and_one_colon(self.src_mac)
+        self.dst_mac = self.format_mac_with_no_hyphens_and_one_colon(self.dst_mac)
+        self.action = self.convert_action_to_provision_format(self.action)
+
+    def format_mac_with_no_hyphens_and_one_colon(self, mac):
+        mac_no_colons = re.sub(':', '', mac)
+        mac_with_hyphen = mac_no_colons[:6] + '-' + mac_no_colons[6:]
+        return mac_with_hyphen
+
+    def convert_action_to_provision_format(self, action):
+        if (action == 'INPORT'):
+            return 'Ingress Port'
+        if (action == 'TABLE'):
+            return 'Table'
+        if (action == 'NORMAL'):
+            return 'Normal'
+        if (action == 'FLOOD'):
+            return 'Flood'
+        if (action == 'ALL'):
+            return 'All'
+        if (action == 'CONTROLLER'):
+            return 'Controller Port'
+        if (action == 'LOCAL'):
+            return 'Local'
+
+    @property
+    def datapath_id_output_command(self):
+        return \
+            'show openflow instance ' + self.of_instance_id + ' | include Datapath'
+
+    connection_index = ''
+
+    def set_connection_index(self, idx):
+        self.connection_index = idx
+
+    datapath_id_output_string = ''
+    datapath_id = ''
+
+    def update_datapath_id(self):
+        if not self.datapath_id_output_string:
+            self.datapath_id = 'unknown'
+        else:
+         #Datapath ID              : 000af0921c22bac0
+         #|-----------------(0)---------------------|
+         #|-----------(1)----------| |------(2)-----|
+         matches = re.search('(.*: )(\w+)', self.datapath_id_output_string)
+         datapath_id_hex = matches.group(2)
+         self.datapath_id = self.convert_hex_to_decimal_as_string(datapath_id_hex)
