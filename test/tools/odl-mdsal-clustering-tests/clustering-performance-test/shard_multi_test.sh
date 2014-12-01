4e523e47967bdba5e0a019287f358935fed67d36
Jan Medved <jmedved@cisco.com>
2014-11-30 21:13:36 -0800

    - Updated multi-blaster.sh and shard_multi_test.sh to allow host and port command line arguments.
    - Updated shard_multi_test.sh to print help string and to measure the total rate.
    
    Change-Id: I6ce4b5a6ad194bf7f28e3d04b2f4dc831a304814
    Signed-off-by: Jan Medved <jmedved@cisco.com>

--- test/tools/odl-mdsal-clustering-tests/clustering-performance-test/shard_multi_test.sh
+++ test/tools/odl-mdsal-clustering-tests/clustering-performance-test/shard_multi_test.sh
@@ -1,22 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 # author__ = "Jan Medved"
 # copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
 # license__ = "New-style BSD"
 # email__ = "jmedved@cisco.com"
 
-# Initialize our own variables:
-instances=0
+# Command to invoke
+CMD="./shard_perf_test.py"
+
+# Default number od $CMD instances to start
+instances=1
+
+# Default parameters for $CMD
 resource="both"
 auth=false
 threads=1
 requests=1000
+odl_host=127.0.0.1
+odl_port=8181
 
-while getopts "h?ai:n:r:t:" opt; do
+function usage {
+    echo "usage: $programname [-h?an] [-i instances] [-c cycles] [-f flows] [-t threads] [-o odl_host] [-p odl_port]"
+    echo "	-h|?          print this message"
+    echo "	-a            use default authentication ('admin/admin')"
+    echo "	-n            use the 'no-delete' flag in '$CMD'"
+    echo "	-i instances  number of '$CMD' instances to spawn"
+    echo "	-c cycles     number of cycles"
+    echo "	-f flows      number of flows"
+    echo "	-o odl_host   IP Address of the ODL controller"
+    echo "	-p odl_port   RESTCONF port in the ODL controller"
+    echo "	-t threads    number of threads"
+    echo "Optional flags/arguments [acfnopt] are passed to '$CMD'."
+}
+
+
+while getopts "h?ai:n:o:p:r:t:" opt; do
     case "$opt" in
     h|\?)
-        echo "This would be help"
-        exit 0
+        usage
+        exit 1
         ;;
     a)  auth=true
         ;;
@@ -26,25 +48,40 @@ while getopts "h?ai:n:r:t:" opt; do
         ;;
     r)  resource=$OPTARG
         ;;
+    o)  odl_host=$OPTARG
+        ;;
+    p)  odl_port=$OPTARG
+        ;;
     t)  threads=$OPTARG
         ;;
     esac
 done
 
-shift $((OPTIND-1))
 
-[ "$1" = "--" ] && shift
-
-echo "Running $instances instances, parameters:\n  resource='$resource', requests=$requests, threads=$threads"
+echo "*** Creating $instances instance(s) of '$CMD' ***"
 
 i=0
+START_TIME=$SECONDS
+
 while [  $i -lt $instances ]; do
-    echo "Starting instance $i"
+    CMD_STRING=$(printf '%s --resource %s --requests %s --threads %s' $CMD $resource $requests $threads)
+    CMD_STRING+=$(printf ' --host %s --port %s' $odl_host $odl_port)
+    if [ "$auth" = true ] ; then
+        CMD_STRING+=' --auth'
+    fi
+    echo "Starting instance $i: '$CMD_STRING'"
+    $CMD_STRING &
+
     let i=$i+1
-    ./shard_perf_test.py --auth --resource $resource --requests $requests --threads $threads &
 done
 
 wait
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
 echo "Done."
 
-# End of file
+if [ "$ELAPSED_TIME" -gt 0 ] ; then
+    let "rate=(threads * $requests * $instances)/$ELAPSED_TIME"
+    echo "Measured rate: $rate"
+    echo "Measured time: $ELAPSED_TIME"
+fi
