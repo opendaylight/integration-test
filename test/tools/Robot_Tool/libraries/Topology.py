cdcd89e17e63e44509483406b954d6f45f6f4895
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-03-16 00:00:19 -0400

    Fix pep8 violations in Topology
    Change-Id: I9fb6f2f2279ab28fb543e2c36c4c235933291d67
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- test/tools/Robot_Tool/libraries/Topology.py
+++ test/tools/Robot_Tool/libraries/Topology.py
@@ -3,27 +3,30 @@ Library for the robot based system test tool of the OpenDaylight project.
 Authors: Baohua Yang@IBM, Denghui Huang@IBM
 Updated: 2013-11-10
 """
-import string
-import robot
 from robot.libraries.BuiltIn import BuiltIn
+
 
 class Topology(object):
     '''
     Topology class provide topology database and provide many method to get property of topology.
     '''
-    topo_nodes_db=[[],
-            [{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}],
-            [{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'},{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'},{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'}]]
+    topo_nodes_db = [
+        [],
+        [{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}],
+        [{u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'},
+         {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'},
+         {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'}]]
+
     def __init__(self):
         self.builtin = BuiltIn()
 
-    def get_nodes_from_topology(self,topo_level):
+    def get_nodes_from_topology(self, topo_level):
         '''
         get nodes from topology database by topology tree level
         '''
         if isinstance(topo_level, str) or isinstance(topo_level, unicode):
             if topo_level.isdigit():
-                topo_level=int(topo_level)
+                topo_level = int(topo_level)
                 if topo_level <= 0:
                     return None
                 return self.topo_nodes_db[topo_level]
