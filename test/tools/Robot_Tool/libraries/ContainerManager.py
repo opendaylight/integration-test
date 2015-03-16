f0df131d1a2604096a7e693b036ddd94ac4c05a6
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-03-16 00:04:27 -0400

    Fix pep8 violations in ContainerManager
    Change-Id: Ibb2d6360358e91a0e634a0c9c4b3d4296ff79b37
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- test/tools/Robot_Tool/libraries/ContainerManager.py
+++ test/tools/Robot_Tool/libraries/ContainerManager.py
@@ -7,7 +7,7 @@ Updated: 2013-11-01
 import sys
 
 sys.path.append('..')
-from restlib import *
+from restlib import *  # noqa
 from testmodule import TestModule
 
 sys.path.remove('..')
@@ -16,7 +16,8 @@ sys.path.remove('..')
 class ContainerManager(TestModule):
     """
     Test for the container manager.
-    Start 2-layer tree topology network. e.g., in Mininet, run  'sudo mn --controller=remote,ip=127.0.0.1 --mac --topo tree,2'
+    Start 2-layer tree topology network. e.g., in Mininet, run
+        'sudo mn --controller=remote,ip=127.0.0.1 --mac --topo tree,2'
     """
 
     def __init__(self, restSubContext='/controller/nb/v2/containermanager', user=DEFAULT_USER, password=DEFAULT_PWD,
@@ -45,7 +46,10 @@ class ContainerManager(TestModule):
     def test_container_operations(self, name, body):
         """
         Test subnet operations, like adding and removeing a subnet.
-        >>> ContainerManager().test_container_operations('cont1',{'container':'cont1','flowSpecs': [], 'staticVlan':'10','nodeConnectors':["OF|1@OF|00:00:00:00:00:00:00:01","OF|23@OF|00:00:00:00:00:00:20:21"]})
+        >>> ContainerManager().test_container_operations('cont1',
+                {'container':'cont1','flowSpecs': [], 'staticVlan':'10','nodeConnectors':[
+                "OF|1@OF|00:00:00:00:00:00:00:01",
+                "OF|23@OF|00:00:00:00:00:00:20:21"]})
         True
         """
         return super(self.__class__, self).test_add_remove_operations('containers', 'container', name, body,
