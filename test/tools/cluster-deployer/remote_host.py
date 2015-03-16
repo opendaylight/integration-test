6d56fa6f44c8fed77ab902cdd38bb2e4bcf7da68
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-03-15 23:38:28 -0400

    Fix pep8 violations in cluster-deployer (2/3)
    Change-Id: I8d027e6c72a5fe911d1f298595a0a8f19ca9d3b6
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- test/tools/cluster-deployer/remote_host.py
+++ test/tools/cluster-deployer/remote_host.py
@@ -6,37 +6,37 @@
 
 from SSHLibrary import SSHLibrary
 
+
 class RemoteHost:
-	def __init__(self, host, user, password, rootdir):
-		self.host = host
-		self.user = user
-		self.password = password
-		self.rootdir = rootdir
+    def __init__(self, host, user, password, rootdir):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.rootdir = rootdir
 
-	def exec_cmd(self, command):
-		print "Executing command " + command + " on host " + self.host
-		lib = SSHLibrary()
-		lib.open_connection(self.host)
-		lib.login(username=self.user,password=self.password)
-		rc = lib.execute_command(command, return_rc = True)
-		lib.close_connection()
-		if rc[1] != 0:
-		   raise Exception('remote command failed [{0}] with exit code {1}'.format(command, rc))
+    def exec_cmd(self, command):
+        print "Executing command " + command + " on host " + self.host
+        lib = SSHLibrary()
+        lib.open_connection(self.host)
+        lib.login(username=self.user, password=self.password)
+        rc = lib.execute_command(command, return_rc=True)
+        lib.close_connection()
+        if rc[1] != 0:
+            raise Exception('remote command failed [{0}] with exit code {1}'.format(command, rc))
 
+    def mkdir(self, dir_name):
+        self.exec_cmd("mkdir -p " + dir_name)
 
-	def mkdir(self, dir_name):
-		self.exec_cmd("mkdir -p " + dir_name)
+    def copy_file(self, src, dest):
+        lib = SSHLibrary()
+        lib.open_connection(self.host)
+        lib.login(username=self.user, password=self.password)
+        print "Copying " + src + " to " + dest + " on " + self.host
+        lib.put_file(src, dest)
+        lib.close_connection()
 
-	def copy_file(self, src, dest):
-	    lib = SSHLibrary()
-	    lib.open_connection(self.host)
-	    lib.login(username=self.user, password=self.password)
-	    print "Copying " + src + " to " + dest + " on " + self.host
-	    lib.put_file(src, dest)
-	    lib.close_connection()
+    def kill_controller(self):
+        self.exec_cmd("ps axf | grep karaf | grep -v grep | awk '{print \"kill -9 \" $1}' | sh")
 
-	def kill_controller(self):
-	    self.exec_cmd("ps axf | grep karaf | grep -v grep | awk '{print \"kill -9 \" $1}' | sh")
-
-	def start_controller(self, dir_name):
-		self.exec_cmd(dir_name + "/odl/bin/start")
+    def start_controller(self, dir_name):
+        self.exec_cmd(dir_name + "/odl/bin/start")
