39082759d211efbd904eab9c8a4c24be9160713d
Patrick Laurin <plaurin@inocybe.com>
2015-03-31 14:27:17 -0400

    Adding libnetconf and netopeer to fedora
    Change-Id: I8e39f9b94f1be47cb4053c79a28c872b05693266
    Signed-off-by: Patrick Laurin <plaurin@inocybe.com>

--- test/tools/VM_Tool/scripts/fedora.sh
+++ test/tools/VM_Tool/scripts/fedora.sh
@@ -4,10 +4,56 @@
 # External shell provisioner script for Fedora
 # --------------------------------------------
 
-# Add EPEL repo for access to Puppet, git-review, etc.
-sudo yum install -y epel-release
-
-# Install other packages (must be done after EPEL repo add)
+# Install initial packages
 sudo yum install -y \
   puppet \
   git
+
+#-----------------
+# Install netopeer
+#-----------------
+
+# Install required system dependencies
+sudo yum install -y \
+  readline \
+  readline-devel \
+  libssh2 \
+  libssh2-devel \
+  libxml2 \
+  libxml2-devel \
+  libxml2-python \
+  libxslt \
+  libxslt-devel \
+  libcurl \
+  libcurl-devel \
+  dbus \
+  dbus-devel \
+  libevent \
+  libevent-devel \
+  libssh-devel \
+  libtool
+
+# Install pyang (extensible YANG validator and converter in python)
+git clone https://github.com/mbj4668/pyang.git && \
+  cd pyang && \
+  sudo python setup.py install
+
+# Install libnetconf (NETCONF library in C)
+git clone https://code.google.com/p/libnetconf && \
+  cd libnetconf && \
+  ./configure  --with-nacm-recovery-uid=1000 && \
+  make && \
+  sudo make install
+
+# Create softlink for libnetconf library in lib64
+sudo ln -s /usr/local/lib/libnetconf.so.0 /lib64
+
+# Install netopeer (set of NETCONF tools built on the libnetconf library)
+git clone https://code.google.com/p/netopeer && \
+  cd netopeer/server && \
+  ./configure && \
+  make && \
+  sudo make install
+
+# Add path to python site-package directory
+echo "/usr/local/lib/python2.7/site-packages" | sudo tee /usr/lib/python2.7/site-packages/netopeer.pth
