98420532b339abc44b174c79c153ee570d922fec
Luis Gomez <ecelgp@gmail.com>
2015-05-26 21:15:12 -0700

    Temporary test to see if we can run a script stored in integration repo
    Change-Id: I53f8aec424d9b615f1f53f206cbb3e77ecb4e376
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

---
+++ test/csit/suites/integration/scripts/sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Controller IP is ${CONTROLLER0}"
+echo "Mininet IP is ${MININET0}"
+echo "Distribution bundle URL is ${ACTUALBUNDLEURL}"
+echo "Distribution bundle is ${BUNDLE}"
+echo "Distribution bundle version is ${BUNDLEVERSION}"
+echo "Distribution folder is ${BUNDLEFOLDER}"
+echo "Nexus prefix is ${NEXUSURL_PREFIX}"
+
