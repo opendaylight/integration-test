d3fcfd9f261b65f2acf1812d1f820baefa01fe60
Peter Gubka <pgubka@cisco.com>
2015-07-07 12:21:52 +0200

    minor improvement to be able to run the script from any location
    Change-Id: Ice274084144499f4fdab4e3fdcb360e59f2204c3
    Signed-off-by: Peter Gubka <pgubka@cisco.com>
    (cherry picked from commit 0a43b22bf933781dd2b7ff33bd12ea1571c6400e)

--- test/tools/odl-mdsal-clustering-tests/clustering-performance-test/ovs-scripts/get-total-reported.sh
+++ test/tools/odl-mdsal-clustering-tests/clustering-performance-test/ovs-scripts/get-total-reported.sh
@@ -7,18 +7,18 @@
 
 ECHO=`which echo`
 TOTAL=0
-
+dname=`dirname $0`
 i=1
 
 while true;
 do
-    CURSTRING=`./OVS-dump-tables.sh.13 s$i 2> /dev/null | grep -v "active=0" | grep "active"`
+    CURSTRING=`${dname}/OVS-dump-tables.sh.13 s$i 2> /dev/null | grep -v "active=0" | grep "active"`
 
     if [ "$CURSTRING" = "" ];
-    then 
-	break
+    then
+        break
     else
-        CUR=`echo $CURSTRING | awk -F'[:=,]' '{print $3}'` 
+        CUR=`echo $CURSTRING | awk -F'[:=,]' '{print $3}'`
         TOTAL=$(($TOTAL + $CUR))
         printf "Switch s%d:\n" $i
         echo "  Table " $CURSTRING
@@ -27,3 +27,4 @@ do
 done
 
 printf "\nTotal: %d\n\n" $TOTAL
+
