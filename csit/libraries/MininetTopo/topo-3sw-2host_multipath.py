80ce34ee72306ae8b7f2a31851ca48e9e34ed85f
priya.ramasubbu <priya.ramasubbu@hcl.com>
2015-09-03 19:03:38 +0530

    PathMap and PathPolicy VTN Manager Sandbox testing.
        * Add new files mininet topology and pathpolicy test cases
    
    Change-Id: I7a6ea77645b9414fe95a582209925272616714c8
    Signed-off-by: priya.ramasubbu <priya.ramasubbu@hcl.com>

---
+++ csit/libraries/MininetTopo/topo-3sw-2host_multipath.py
@@ -0,0 +1,39 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+
+class PathpolicyTopo(Topo):
+    """Simple topology example."""
+
+    def __init__(self):
+
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Add hosts and switches
+        leftHost = self.addHost('h1')
+        rightHost = self.addHost('h2')
+        leftSwitch = self.addSwitch('s1')
+        middleSwitch = self.addSwitch('s2')
+        middleSwitch2 = self.addSwitch('s4')
+        rightSwitch = self.addSwitch('s3')
+
+        # Add links
+        self.addLink(leftHost, leftSwitch)
+        self.addLink(leftSwitch, middleSwitch)
+        self.addLink(leftSwitch, middleSwitch2)
+        self.addLink(middleSwitch, rightSwitch)
+        self.addLink(middleSwitch2, rightSwitch)
+        self.addLink(rightSwitch, rightHost)
+
+
+topos = {'pathpolicytopo': (lambda: PathpolicyTopo())}
