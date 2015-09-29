c162a0165c929bba65fc4da6f4a38a2503081204
Martin Sunal <msunal@cisco.com>
2015-09-30 00:10:32 +0200

    GBP Robot, new structure
    Change-Id: Ief74596257a13243f22b62b8c51ff3af38687e38
    Signed-off-by: Konstantin Blagov <kblagov@cisco.com>
    Signed-off-by: Igor Bartak <ibartak@cisco.com>
    Signed-off-by: Martin Sunal <msunal@cisco.com>
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>
    Signed-off-by: Dileep <dileep.ranganathan@intel.com>

---
+++ csit/suites/groupbasedpolicy/common_scripts/clean-demo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+sw=$(sudo ovs-vsctl show | egrep -E 'Bridge.*sw' | awk '{print $2}' | sed -e 's/^"//'  -e 's/"$//')
+
+if [ "$sw" ]
+then
+    sudo ovs-vsctl del-br $sw
+fi
+
+sudo ovs-vsctl del-manager;
+docker stop -t=1 $(docker ps -a -q) > /dev/null 2>&1
+docker rm $(docker ps -a -q) > /dev/null 2>&1
