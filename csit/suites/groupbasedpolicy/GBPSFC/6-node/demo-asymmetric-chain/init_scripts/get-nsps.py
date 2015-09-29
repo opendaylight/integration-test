91c7f2937becb2c99519163a47851eb582b674be
Martin Sunal <msunal@cisco.com>
2015-09-30 00:10:32 +0200

    WIP - DO NOT MERGE - GBP Robot, new structure Fix - conform to the PEP 8 style
    Change-Id: Ief74596257a13243f22b62b8c51ff3af38687e38
    Signed-off-by: Konstantin Blagov <kblagov@cisco.com>
    Signed-off-by: Igor Bartak <ibartak@cisco.com>
    Signed-off-by: Martin Sunal <msunal@cisco.com>
    Signed-off-by: Konstantin Blagov <kblagov@cisco.com>
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>
    Signed-off-by: Konstantin Blagov <kblagov@cisco.com>
    Signed-off-by: Dileep <dileep.ranganathan@intel.com>

---
+++ csit/suites/groupbasedpolicy/GBPSFC/6-node/demo-asymmetric-chain/init_scripts/get-nsps.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+# for asymmetric chain
+import socket
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+import sys
+import os
+from subprocess import check_output
+from infrastructure_config import switches
+
+DEFAULT_PORT = '8181'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+
+def get(host, port, uri):
+    url = 'http://'+host+":"+port+uri
+    # print url
+    r = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    jsondata = json.loads(r.text)
+    return jsondata
+
+
+def get_rsps_uri():
+    return "/restconf/operational/rendered-service-path:rendered-service-paths"
+
+
+def doCmd(cmd):
+    listcmd = cmd.split()
+    print check_output(listcmd)
+
+
+if __name__ == "__main__":
+    # Launch main menu
+    # Some sensible defaults
+    controller = os.environ.get('ODL')
+    if controller is None:
+        sys.exit("No controller set.")
+    # else:
+    # print "Contacting controller at %s" % controller
+
+    resp = get(controller, DEFAULT_PORT, get_rsps_uri())
+    if len(resp['rendered-service-paths']) > 0:
+        paths = resp['rendered-service-paths']['rendered-service-path']
+
+        nsps = []
+        for path in paths:
+            nsps.append(path['path-id'])
+        if len(nsps) > 0:
+            sw_index = int(socket.gethostname().split("gbpsfc", 1)[1])-1
+            if sw_index in range(0, len(switches)+1):
+
+                controller = os.environ.get('ODL')
+                sw_type = switches[sw_index]['type']
+                sw_name = switches[sw_index]['name']
+                if sw_type == 'sf':
+                    print "******************************"
+                    print "Adding flows for %s as an SF." % sw_name
+                    print "******************************"
+                    doCmd('sudo /vagrant/utils/sf-flows.sh %s' % min(nsps))
