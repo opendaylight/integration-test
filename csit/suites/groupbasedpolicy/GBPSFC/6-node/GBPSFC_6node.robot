6952a5a44425466b98ed8ddec46d5c06249c7bee
Tomas Cechvala <tcechval@cisco.com>
2015-10-06 11:41:45 +0200

    Changes of robot and bash scripts for GBPSFC demo
    Symmetric and asymmetric demo
    
    Change-Id: I4f2ca1ec863607a4ad99cf754c17cb10979b7da6
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- csit/suites/groupbasedpolicy/GBPSFC/6-node/GBPSFC_6node.robot
+++ csit/suites/groupbasedpolicy/GBPSFC/6-node/GBPSFC_6node.robot
@@ -16,12 +16,15 @@ Setup Node
     SSHLibrary.Execute Command
     ...    mv ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}/init_scripts/* ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}
     SSHLibrary.Execute Command    rm -rf ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}/init_scripts/
-    ${stderr}    SSHLibrary.Execute Command    sudo ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}/infrastructure_launch.py
+    ${stderr}    SSHLibrary.Execute Command
+    ...    ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}/execute-in-ve.sh python ${VM_HOME_FOLDER}${/}${VM_SCRIPTS_FOLDER}/infrastructure_launch.py
     ...    return_stderr=True    return_stdout=False
+    Should Be Empty    ${stderr}
     Run Keyword If    "${GBPSFC}" == "${GBPSFC3}"
-    ...    Run Keywords    Render SF Flows    ${GBPSFC2}    ${stderr}    AND    Should Be Empty    ${stderr}
+    ...    Render SF Flows    ${GBPSFC2}    ${stderr}
     ...    ELSE IF    "${GBPSFC}" == "${GBPSFC5}"
-    ...    Run Keywords    Render SF Flows    ${GBPSFC4}    ${stderr}    AND    Should Be Empty    ${stderr}
+    ...    Render SF Flows    ${GBPSFC4}    ${stderr}
+    Should Be Empty    ${stderr}
     SSHLibrary.Close Connection
 
 Render SF Flows
--- csit/suites/groupbasedpolicy/common_scripts/infrastructure_launch.py
+++ csit/suites/groupbasedpolicy/common_scripts/infrastructure_launch.py
@@ -12,17 +12,17 @@ from infrastructure_config import defaultContainerImage
 
 
 def addController(sw, ip):
-    call(['ovs-vsctl', 'set-controller', sw, 'tcp:%s:6653' % ip])
+    call(['sudo', 'ovs-vsctl', 'set-controller', sw, 'tcp:%s:6653' % ip])
 
 
 def addManager(ip):
-    cmd = "ovs-vsctl set-manager tcp:%s:6640" % ip
+    cmd = "sudo ovs-vsctl set-manager tcp:%s:6640" % ip
     listcmd = cmd.split()
     print check_output(listcmd)
 
 
 def addSwitch(name, dpid=None):
-    call(['ovs-vsctl', 'add-br', name])  # Add bridge
+    call(['sudo', 'ovs-vsctl', 'add-br', name])  # Add bridge
     if dpid:
         if len(dpid) < 16:  # DPID must be 16-bytes in later versions of OVS
             filler = '0000000000000000'
@@ -30,7 +30,7 @@ def addSwitch(name, dpid=None):
         elif len(dpid) > 16:
             print 'DPID: %s is too long' % dpid
             sys.exit(3)
-        call(['ovs-vsctl', 'set', 'bridge', name,
+        call(['sudo', 'ovs-vsctl', 'set', 'bridge', name,
               'other-config:datapath-id=%s' % dpid])
 
 
@@ -40,12 +40,12 @@ def addHost(net, switch, name, ip, mac):
 
 
 def setOFVersion(sw, version='OpenFlow13,OpenFlow12,OpenFlow10'):
-    call(['ovs-vsctl', 'set', 'bridge', sw, 'protocols={}'.format(version)])
+    call(['sudo', 'ovs-vsctl', 'set', 'bridge', sw, 'protocols={}'.format(version)])
 
 
 def addTunnel(sw, sourceIp=None):
     ifaceName = '{}-vxlan-0'.format(sw)
-    cmd = ['ovs-vsctl', 'add-port', sw, ifaceName,
+    cmd = ['sudo', 'ovs-vsctl', 'add-port', sw, ifaceName,
            '--', 'set', 'Interface', ifaceName,
            'type=vxlan',
            'options:remote_ip=flow',
@@ -57,7 +57,7 @@ def addTunnel(sw, sourceIp=None):
 
 def addGpeTunnel(sw, sourceIp=None):
     ifaceName = '{}-vxlangpe-0'.format(sw)
-    cmd = ['ovs-vsctl', 'add-port', sw, ifaceName,
+    cmd = ['sudo', 'ovs-vsctl', 'add-port', sw, ifaceName,
            '--', 'set', 'Interface', ifaceName,
            'type=vxlan',
            'options:remote_ip=flow',
@@ -98,16 +98,18 @@ def connectContainerToSwitch(sw, host, containerID, of_port):
     nw = ipaddr.IPv4Network(hostIP)
     broadcast = "{}".format(nw.broadcast)
     router = "{}".format(nw.network + 1)
-    cmd = [
-        '/vagrant/ovswork.sh',
-        sw,
-        containerID,
-        hostIP,
-        broadcast,
-        router,
-        mac,
-        of_port,
-        host['name']]
+    ovswork_path = os.path.dirname(os.path.realpath(__file__)) + '/ovswork.sh'
+    # TODO remove 'sudo' from the command below
+    cmd = ['sudo',
+           ovswork_path,
+           sw,
+           containerID,
+           hostIP,
+           broadcast,
+           router,
+           mac,
+           of_port,
+           host['name']]
     if ('vlan') in host:
         cmd.append(host['vlan'])
     call(cmd)
@@ -164,7 +166,7 @@ if __name__ == "__main__":
             print "OVS status:"
             print "-----------"
             print
-            doCmd('ovs-vsctl show')
+            doCmd('sudo ovs-vsctl show')
             print
             print "Docker containers:"
             print "------------------"
