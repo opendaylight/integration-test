46470218db71de2de0f52439e38668eaba5fbd4a
Radovan Sajben <radovan.sajben@pantheon.sk>
2015-10-08 13:58:49 +0200

    MD-SAL Data Store benchmarking.
    - mdsal/dsbenchmark performance testsuite introduced
    - update of the dsbenchmark.py tool (output suitable for plotting)
    
    Change-Id: I91f4f57e8ba0e52b7671266490d773ea5eb38b9d
    Signed-off-by: Radovan Sajben <radovan.sajben@pantheon.sk>

--- tools/mdsal_benchmark/dsbenchmark.py
+++ tools/mdsal_benchmark/dsbenchmark.py
@@ -9,6 +9,7 @@ import requests
 import json
 import csv
 import time
+import re
 
 parser = argparse.ArgumentParser(description='Datastore Benchmarking'
                                              ''
@@ -24,9 +25,9 @@ parser.add_argument("--port", type=int, default=8181, help="the port number of t
 parser.add_argument("--txtype", choices=["TX-CHAINING", "SIMPLE-TX"], nargs='+', default=["TX-CHAINING", "SIMPLE-TX"],
                     help="list of the transaction types to execute.")
 parser.add_argument("--total", type=int, default=100000, help="total number of elements to process.")
-parser.add_argument("--inner", type=int, default=[1, 10, 100, 1000, 10000, 100000],
+parser.add_argument("--inner", type=int, default=[1, 10, 100, 1000, 10000, 100000], nargs='+',
                     help="number of inner elements to process.")
-parser.add_argument("--ops", type=int, default=[1, 10, 100, 1000, 10000, 100000],
+parser.add_argument("--ops", type=int, default=[1, 10, 100, 1000, 10000, 100000], nargs='+',
                     help="number of operations per transaction.")
 parser.add_argument("--optype", choices=["PUT", "MERGE", "DELETE", "READ"], nargs='+',
                     default=["PUT", "MERGE", "DELETE", "READ"], help="list of the types operations to execute.")
@@ -35,6 +36,10 @@ parser.add_argument("--format", choices=["BINDING-AWARE", "BINDING-INDEPENDENT"]
 parser.add_argument("--warmup", type=int, default=10, help="number of warmup runs before official test runs")
 parser.add_argument("--runs", type=int, default=10,
                     help="number of official test runs. Note: Reported results are based on these runs.")
+parser.add_argument("--plot", type=str, default='none',
+                    help="keywords filter for results to be drawn in plot (special keywords: all, none).")
+parser.add_argument("--units", choices=["miliseconds", "microseconds"], default="microseconds",
+                    help="units of test duration values provided by dsbenchmark controller feature")
 args = parser.parse_args()
 
 
@@ -135,6 +140,49 @@ def run_test(warmup_runs, test_runs, tx_type, operation, data_fmt, outer_elem, i
     return total_build_time / test_runs, total_exec_time / test_runs
 
 
+def store_result(values, tx_type, operation, data_fmt, outer_elem, inner_elem, ops_per_tx, value_name, value):
+    """
+    Stores a record to the list (dictionary) of values to be written into a csv file for plotting purposes.
+    :param values: The list (dictionary) to be used for storing the result
+    :param operation: PUT, MERGE or DELETE
+    :param data_fmt: BINDING-AWARE or BINDING-INDEPENDENT
+    :param outer_elem: Number of elements in the outer list
+    :param inner_elem: Number of elements in the inner list
+    :param ops_per_tx: Number of operations (PUTs, MERGEs or DELETEs) on each transaction
+    :param value_name: Value name (name for the measured value)
+    :param value: The (measured) value
+    :return: none
+    """
+    plot_key = (tx_type + '-' + data_fmt + '-' + operation + '-' + str(outer_elem) + '/' +
+                str(inner_elem) + 'OUTER/INNER-' + str(ops_per_tx) + 'OP-' + value_name)
+    values[plot_key] = value
+
+
+def write_results_to_file(values, file_name, key_filter):
+    """
+    Writes specified results (values) into the file (file_name). Results are filtered according to key_filter value.
+    Produces a csv file consumable by Jnekins integration environment.
+    :param file_name: Name of the (csv) file to be created
+    :param values: The list (dictionary) to be written into the file
+    :param key_filter: A regexp string to filter the results to be finally put into the file
+    :return: none
+    """
+    first_line = ''
+    second_line = ''
+    f = open(file_name, 'wt')
+    try:
+        for key in sorted(values):
+            if (key_filter != 'none') & ((key_filter == 'all') | (re.search(key_filter, key) is not None)):
+                first_line += key + ', '
+                second_line += str(values[key]) + ', '
+        first_line = first_line[:-2]
+        second_line = second_line[:-2]
+        f.write(first_line + '\n')
+        f.write(second_line + '\n')
+    finally:
+        f.close()
+
+
 if __name__ == "__main__":
     # Test Parameters
     TX_TYPES = args.txtype
@@ -143,6 +191,15 @@ if __name__ == "__main__":
     OPS_PER_TX = args.ops
     OPERATIONS = args.optype
     DATA_FORMATS = args.format
+    PLOT_FILTER = args.plot
+    if args.units == 'miliseconds':
+        TIME_DIV = 1
+    else:
+        TIME_DIV = 1000
+
+    # Dictionaries for storing keys & values to plot
+    PLOT1 = {}
+    PLOT2 = {}
 
     # Iterations
     WARMUP_RUNS = args.warmup
@@ -187,6 +244,10 @@ if __name__ == "__main__":
                         e_label = '%d/%d' % (TOTAL_ELEMENTS / elem, elem)
                         writer.writerow(('', '', '', e_label, avg_build_time, avg_exec_time,
                                          (avg_build_time + avg_exec_time)))
+                        store_result(PLOT1, tx_type, oper, fmt, TOTAL_ELEMENTS / elem, elem, 1,
+                                     'BUILD', avg_build_time / TIME_DIV)
+                        store_result(PLOT1, tx_type, oper, fmt, TOTAL_ELEMENTS / elem, elem, 1,
+                                     'EXEC', avg_exec_time / TIME_DIV)
 
         # Determine the impact of number of writes per transaction on performance.
         # Iterate over all transaction types, data formats, operation types, and
@@ -215,6 +276,13 @@ if __name__ == "__main__":
                             run_test(WARMUP_RUNS, TEST_RUNS, tx_type, oper, fmt, TOTAL_ELEMENTS, 1, wtx)
                         writer.writerow(('', '', '', wtx, avg_build_time, avg_exec_time,
                                          (avg_build_time + avg_exec_time)))
+                        store_result(PLOT2, tx_type, oper, fmt, TOTAL_ELEMENTS / elem, 1, wtx,
+                                     'BUILD', avg_build_time / TIME_DIV)
+                        store_result(PLOT2, tx_type, oper, fmt, TOTAL_ELEMENTS / elem, 1, wtx,
+                                     'EXEC', avg_exec_time / TIME_DIV)
+
+        write_results_to_file(PLOT1, 'perf_per_struct.csv', PLOT_FILTER)
+        write_results_to_file(PLOT2, 'perf_per_ops.csv', PLOT_FILTER)
 
         end_time = time.time()
         print "End time: %f " % end_time
