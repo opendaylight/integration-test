33b9d8957792347291bcc858caa675115023ae92
Martin Mihálek <mamihale@cisco.com>
2015-11-05 13:45:59 +0100

    Bug 4567 - CSIT config xml contains outdated config
    For Lithium branch notification-service option is added to config file
    
    Change-Id: If3b45cb0e6305b5941fd190417d77e8257004c7d
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

--- csit/suites/sxp/scripts/topology_node_setup.sh
+++ csit/suites/sxp/scripts/topology_node_setup.sh
@@ -3,4 +3,9 @@
 echo "Setup config to ${ODL_SYSTEM_IP}"
 ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
 
+if [ "${BRANCH#*/}" == "lithium" ]; then
+    LI_FIX="<notification-service>\n<type xmlns:binding=\"urn:opendaylight:params:xml:ns:yang:controller:md:sal:binding\">\nbinding:binding-notification-service\n</type>\n<name>binding-notification-broker</name>\n</notification-service>";
+    sed -i "36i $LI_FIX" ${WORKSPACE}/test/csit/suites/sxp/basic/22-sxp-controller-one-node.xml;
+fi
+
 scp ${WORKSPACE}/test/csit/suites/sxp/topology/22-sxp-controller-one-node.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/
