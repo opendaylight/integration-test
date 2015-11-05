041a6230b8f44ceb8e1bf91f56242f41a928f687
Martin Mihálek <mamihale@cisco.com>
2015-11-05 13:45:59 +0100

    Bug 4567 - CSIT config xml contains outdated config
    For Lithium branch notification-service option is added to config file
    
    Change-Id: If3b45cb0e6305b5941fd190417d77e8257004c7d
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

--- csit/suites/sxp/scripts/topology_node_setup.sh
+++ csit/suites/sxp/scripts/topology_node_setup.sh
@@ -3,4 +3,8 @@
 echo "Setup config to ${ODL_SYSTEM_IP}"
 ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
 
-scp ${WORKSPACE}/test/csit/suites/sxp/topology/22-sxp-controller-one-node.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/
+if [ "${BRANCH#*/}" == "lithium" ]; then
+    scp ${WORKSPACE}/test/csit/suites/sxp/topology/22-sxp-controller-one-node-lithium.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/22-sxp-controller-one-node.xml
+else
+    scp ${WORKSPACE}/test/csit/suites/sxp/topology/22-sxp-controller-one-node.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/
+fi
