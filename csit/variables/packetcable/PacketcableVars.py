65f6d9248c870094e78cd9441926f017b0d66d99
Ryan Vail <r.vail@cablelabs.com>
2015-11-13 14:02:57 -0700

    updated packetcable csit test for changed api
    -Added csit/variables/packetcable/PacketcableVars.py to handle variable setting based on a new
    ODL_VERSION option that can (optionaly) be supplied to robot. Defaults to master if not set.
    -Resources split into two groups lithium and master.
    -Test cases updated to use new variables so both Li and Be can be tested
    
    Change-Id: Icd397f9122ddc9bc159ec6956a32496e349ae1d4
    Signed-off-by: Ryan Vail <r.vail@cablelabs.com>

---
+++ csit/variables/packetcable/PacketcableVars.py
@@ -0,0 +1,27 @@
+def extract_if_list(arg):
+    if isinstance(arg, list):
+        return arg[0]
+    else:
+        return arg
+
+
+def get_variables(version, curr_dir):
+    # if not provided to robot via commandline the default value seems to
+    # be a list containing the version
+    version = extract_if_list(version)
+    curr_dir = extract_if_list(curr_dir)
+
+    # set default variable values
+    variables = {
+        'ODLREST_CAPPS': '/restconf/config/packetcable:ccaps',
+        'CCAP_TOKEN': 'ccap',
+        'PACKETCABLE_RESOURCE_DIR':
+            ''.join([curr_dir, "/../../../variables/packetcable/", version])
+    }
+
+    # override variables for specific versions
+    if version == 'lithium':
+        variables['ODLREST_CAPPS'] = '/restconf/config/packetcable:ccap'
+        variables['CCAP_TOKEN'] = 'ccaps'
+
+    return variables
