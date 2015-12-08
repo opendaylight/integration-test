b33ef276e5776b170dc09caefd44b97ec144b7c0
Pavol Cibak <pavol.cibak@pantheon.sk>
2015-12-08 17:31:22 +0100

    Rework manage_play.py to use threads insead of processes, better code cosmetic, fixed logger
    Change-Id: I9f5e9cdb7821dd26aac276398369410a96b39d83
    Signed-off-by: Pavol Cibak <pavol.cibak@pantheon.sk>

--- tools/fastbgp/manage_play.py
+++ tools/fastbgp/manage_play.py
@@ -1,4 +1,4 @@
-"""Utility for running several play.py utilities in parallel.
+"""Utility for running several play utilities in parallel in multithreaded mode.
 
 Only a subset of play.py functionality is fully supported.
 Notably, listening play.py is not tested yet.
@@ -21,11 +21,54 @@ __copyright__ = "Copyright(c) 2015, Cisco Systems, Inc."
 __license__ = "Eclipse Public License v1.0"
 __email__ = "vrpolak@cisco.com"
 
-
 import argparse
 import ipaddr
-import multiprocessing
-import subprocess
+import thread
+import binascii
+
+from play import MessageError, MessageGenerator, StateTracker, TimeTracker, establish_connection, \
+    read_open_message, create_logger
+
+
+def job(args):
+    # Create object from dict
+    class Arguments:
+        def __init__(self, **kwargs):
+            self.__dict__.update(kwargs)
+
+    arguments = Arguments(**args)
+    bgp_socket = establish_connection(arguments)
+    # Initial handshake phase. TODO: Can it be also moved to StateTracker?
+    # Receive open message before sending anything.
+    # FIXME: Add parameter to send default open message first,
+    # to work with "you first" peers.
+    msg_in = read_open_message(bgp_socket)
+    timer = TimeTracker(msg_in)
+    generator = MessageGenerator(arguments)
+    msg_out = generator.open_message()
+    stdout_logger = create_logger(1, 'manage.log')
+    stdout_logger.debug("Sending the OPEN message: " + binascii.hexlify(msg_out))
+    # Send our open message to the peer.
+    bgp_socket.send(msg_out)
+    # Wait for confirming keepalive.
+    # TODO: Surely in just one packet?
+    # Using exact keepalive length to not to see possible updates.
+    msg_in = bgp_socket.recv(19)
+    if msg_in != generator.keepalive_message():
+        stdout_logger.error("Open not confirmed by keepalive, instead got " + binascii.hexlify(msg_in))
+        raise MessageError("Open not confirmed by keepalive, instead got", msg_in)
+    timer.reset_peer_hold_time()
+    # Send the keepalive to indicate the connection is accepted.
+    timer.snapshot()  # Remember this time.
+    msg_out = generator.keepalive_message()
+    stdout_logger.debug("Sending a KEEP ALIVE message: " + binascii.hexlify(msg_out))
+    bgp_socket.send(msg_out)
+    # Use the remembered time.
+    timer.reset_my_keepalive_time(timer.snapshot_time)
+    # End of initial handshake phase.
+    state = StateTracker(bgp_socket, generator, timer)
+    while True:  # main reactor loop
+        state.perform_one_loop_iteration()
 
 
 def main():
@@ -42,7 +85,7 @@ def main():
     parser.add_argument("--firstprefix", default="8.0.1.0", type=ipaddr.IPv4Address, help=str_help)
     str_help = "If present, this tool will be listening for connection, instead of initiating it."
     parser.add_argument("--listen", action="store_true", help=str_help)
-    str_help = "How many play.py utilities are to be started."
+    str_help = "How many play utilities are to be started."
     parser.add_argument("--multiplicity", default="1", type=int, help=str_help)
     str_help = "Numeric IP Address to bind to and derive BGP ID from, for the first player."
     parser.add_argument("--myip", default="0.0.0.0", type=ipaddr.IPv4Address, help=str_help)
@@ -66,35 +109,40 @@ def main():
     utils_left = mgr_args.multiplicity
     prefix_current = mgr_args.firstprefix
     myip_current = mgr_args.myip
-    processes = []
+    thread_args = []
+
     while 1:
         amount_per_util = (amount_left - 1) / utils_left + 1  # round up
         amount_left -= amount_per_util
         utils_left -= 1
-        util_args = ["python", "play.py"]
-        util_args.extend(["--asnumber", str(mgr_args.asnumber)])
-        util_args.extend(["--amount", str(amount_per_util)])
-        util_args.extend(["--firstprefix", str(prefix_current)])
-        if mgr_args.listen:
-            util_args.append("--listen")
-        util_args.extend(["--myip", str(myip_current)])
-        util_args.extend(["--myport", str(mgr_args.myport)])
-        util_args.extend(["--nexthop", str(mgr_args.nexthop)])
-        util_args.extend(["--peerip", str(mgr_args.peerip)])
-        util_args.extend(["--peerport", str(mgr_args.peerport)])
-        process = multiprocessing.Process(target=subprocess.call, args=[util_args])
-        # No shell, inherited std* file descriptors, same process group, so gets SIGINT.
-        processes.append(process)
+        args = dict(
+            asnumber=str(mgr_args.asnumber),
+            amount=str(amount_per_util),
+            firstprefix=str(prefix_current),
+            listen=bool(mgr_args.listen),
+            myip=str(myip_current),
+            myport=int(mgr_args.myport),
+            nexthop=str(mgr_args.nexthop),
+            peerip=str(mgr_args.peerip),
+            peerport=int(mgr_args.peerport),
+        )
+        thread_args.append(args)
         if not utils_left:
             break
         prefix_current += amount_per_util * 16
         myip_current += 1
-    for process in processes:
-        process.start()
-    # Usually processes run until SIGINT, but if an error happens we want to exit quickly.
-    for process in processes:
-        process.join()
 
+    try:
+        # Create threads
+        for t in thread_args:
+            thread.start_new_thread(job, (t,))
+    except Exception:
+        print "Error: unable to start thread."
+        raise SystemExit(2)
+
+    # Work remains forever
+    while 1:
+        pass
 
 if __name__ == "__main__":
     main()
