f2431da774a891d94bd83744ce3f17d902101858
Poornachandrarao <poornachandrarao.b@hcl.com>
2015-12-10 14:02:52 -0500

    Do not Merge: devstack up csit test
    Change-Id: If89edbbde8ee6bb508a6ab0a2f7754594e28a283
    Signed-off-by: Poornachandrarao <poornachandrarao.b@hcl.com>

--- csit/variables/Variables.py
+++ csit/variables/Variables.py
@@ -186,3 +186,7 @@ MININET_USER = TOOLS_SYSTEM_USER
 MININET_PASSWORD = TOOLS_SYSTEM_PASSWORD
 MININET_PROMPT = TOOLS_SYSTEM_PROMPT
 PROMPT = '>'  # TODO: remove this as it's vague.  need to fix any occurances of it first.
+
+# Testing local configfile 
+CREATE_LOCALCONF_FILE = "local.conf"
+CREATE_STACK_FILE = "stack.sh"
