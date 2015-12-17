73c1a6ee3ea97f3a85168e8b075b5e5a9fb58880
pavol.cibak@pantheon.sk <pavol.cibak@pantheon.sk>
2015-12-17 09:07:19 +0100

    Rework manage_play.py to use threads instead of processes, better code cosmetic, logger fixed.
    Change-Id: I6be7e4eaf481d903ca493e067cedcc6af7dee685
    Signed-off-by: pavol.cibak@pantheon.sk <pavol.cibak@pantheon.sk>

--- tools/fastbgp/manage_play.py
+++ tools/fastbgp/manage_play.py
@@ -1,4 +1,4 @@
-"""Utility for running several play.py utilities in parallel.
+"""Utility for running several play utilities in parallel in multithreaded mode.
 
 Only a subset of play.py functionality is fully supported.
 Notably, listening play.py is not tested yet.
@@ -21,11 +21,49 @@ __copyright__ = "Copyright(c) 2015, Cisco Systems, Inc."
 __license__ = "Eclipse Public License v1.0"
 __email__ = "vrpolak@cisco.com"
 
-
 import argparse
 import ipaddr
-import multiprocessing
-import subprocess
+import thread
+import binascii
+import time
+import logging
+
+from play import MessageError, MessageGenerator, StateTracker, TimeTracker, establish_connection, \
+    read_open_message, logger
+
+
+def job(arguments):
+    bgp_socket = establish_connection(arguments)
+    # Initial handshake phase. TODO: Can it be also moved to StateTracker?
+    # Receive open message before sending anything.
+    # FIXME: Add parameter to send default open message first,
+    # to work with "you first" peers.
+    msg_in = read_open_message(bgp_socket)
+    timer = TimeTracker(msg_in)
+    generator = MessageGenerator(arguments)
+    msg_out = generator.open_message()
+    logger.debug("Sending the OPEN message: " + binascii.hexlify(msg_out))
+    # Send our open message to the peer.
+    bgp_socket.send(msg_out)
+    # Wait for confirming keepalive.
+    # TODO: Surely in just one packet?
+    # Using exact keepalive length to not to see possible updates.
+    msg_in = bgp_socket.recv(19)
+    if msg_in != generator.keepalive_message():
+        logger.error("Open not confirmed by keepalive, instead got " + binascii.hexlify(msg_in))
+        raise MessageError("Open not confirmed by keepalive, instead got", msg_in)
+    timer.reset_peer_hold_time()
+    # Send the keepalive to indicate the connection is accepted.
+    timer.snapshot()  # Remember this time.
+    msg_out = generator.keepalive_message()
+    logger.debug("Sending a KEEP ALIVE message: " + binascii.hexlify(msg_out))
+    bgp_socket.send(msg_out)
+    # Use the remembered time.
+    timer.reset_my_keepalive_time(timer.snapshot_time)
+    # End of initial handshake phase.
+    state = StateTracker(bgp_socket, generator, timer)
+    while True:  # main reactor loop
+        state.perform_one_loop_iteration()
 
 
 def main():
@@ -42,7 +80,7 @@ def main():
     parser.add_argument("--firstprefix", default="8.0.1.0", type=ipaddr.IPv4Address, help=str_help)
     str_help = "If present, this tool will be listening for connection, instead of initiating it."
     parser.add_argument("--listen", action="store_true", help=str_help)
-    str_help = "How many play.py utilities are to be started."
+    str_help = "How many play utilities are to be started."
     parser.add_argument("--multiplicity", default="1", type=int, help=str_help)
     str_help = "Numeric IP Address to bind to and derive BGP ID from, for the first player."
     parser.add_argument("--myip", default="0.0.0.0", type=ipaddr.IPv4Address, help=str_help)
@@ -54,6 +92,40 @@ def main():
     parser.add_argument("--peerip", default="127.0.0.2", type=ipaddr.IPv4Address, help=str_help)
     str_help = "TCP port to try to connect to. No effect in listening mode."
     parser.add_argument("--peerport", default="179", type=int, help=str_help)
+
+    str_help = "Maximum number of IP prefixes to be announced in one iteration"
+    parser.add_argument("--insert", default="1", type=int, help=str_help)
+    str_help = "Maximum number of IP prefixes to be withdrawn in one iteration"
+    parser.add_argument("--withdraw", default="0", type=int, help=str_help)
+    str_help = "The number of prefixes to process without withdrawals"
+    parser.add_argument("--prefill", default="0", type=int, help=str_help)
+    str_help = "Single or two separate UPDATEs for NLRI and WITHDRAWN lists sent"
+    parser.add_argument("--updates", choices=["single", "separate"],
+                        default=["separate"], help=str_help)
+    str_help = "The prefix length."
+    parser.add_argument("--prefixlen", default=28, type=int, help=str_help)
+    str_help = "Local hold time."
+    parser.add_argument("--holdtime", default="180", type=int, help=str_help)
+    str_help = "Log level (--error, --warning, --info, --debug)"
+    parser.add_argument("--error", dest="loglevel", action="store_const",
+                        const=logging.ERROR, default=logging.INFO,
+                        help=str_help)
+    parser.add_argument("--warning", dest="loglevel", action="store_const",
+                        const=logging.WARNING, default=logging.INFO,
+                        help=str_help)
+    parser.add_argument("--info", dest="loglevel", action="store_const",
+                        const=logging.INFO, default=logging.INFO,
+                        help=str_help)
+    parser.add_argument("--debug", dest="loglevel", action="store_const",
+                        const=logging.DEBUG, default=logging.INFO,
+                        help=str_help)
+    str_help = "Trailing part of the csv result files for plotting purposes"
+    parser.add_argument("--results", default="bgp.csv", type=str, help=str_help)
+    str_help = "Minimum number of updates to reach to include result into csv."
+    parser.add_argument("--threshold", default="1000", type=int, help=str_help)
+    str_help = "RFC 4760 Multiprotocol Extensions for BGP-4 supported"
+    parser.add_argument("--rfc4760", default="yes", type=str, help=str_help)
+
     # TODO: The step between IP prefixes is currently hardcoded to 16. Should we make it configurable?
     # Yes, the argument list above is sorted alphabetically.
     mgr_args = parser.parse_args()
@@ -66,35 +138,29 @@ def main():
     utils_left = mgr_args.multiplicity
     prefix_current = mgr_args.firstprefix
     myip_current = mgr_args.myip
-    processes = []
+    thread_args = []
+
     while 1:
         amount_per_util = (amount_left - 1) / utils_left + 1  # round up
         amount_left -= amount_per_util
         utils_left -= 1
-        util_args = ["python", "play.py"]
-        util_args.extend(["--asnumber", str(mgr_args.asnumber)])
-        util_args.extend(["--amount", str(amount_per_util)])
-        util_args.extend(["--firstprefix", str(prefix_current)])
-        if mgr_args.listen:
-            util_args.append("--listen")
-        util_args.extend(["--myip", str(myip_current)])
-        util_args.extend(["--myport", str(mgr_args.myport)])
-        util_args.extend(["--nexthop", str(mgr_args.nexthop)])
-        util_args.extend(["--peerip", str(mgr_args.peerip)])
-        util_args.extend(["--peerport", str(mgr_args.peerport)])
-        process = multiprocessing.Process(target=subprocess.call, args=[util_args])
-        # No shell, inherited std* file descriptors, same process group, so gets SIGINT.
-        processes.append(process)
+        thread_args.append(mgr_args)
         if not utils_left:
             break
         prefix_current += amount_per_util * 16
         myip_current += 1
-    for process in processes:
-        process.start()
-    # Usually processes run until SIGINT, but if an error happens we want to exit quickly.
-    for process in processes:
-        process.join()
 
+    try:
+        # Create threads
+        for t in thread_args:
+            thread.start_new_thread(job, (t,))
+    except Exception:
+        print "Error: unable to start thread."
+        raise SystemExit(2)
+
+    # Work remains forever
+    while 1:
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
