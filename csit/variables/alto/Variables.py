5a5f7934ac801f2dbf95ad62c96d812ae0dd4552
Xiao <linxiao9292@outlook.com>
2016-01-14 21:48:22 +0800

    Add integration tests for ALTO project.
    Change-Id: Ic927f9c32b96fdbbca63cdc9caba0d2991fc5e5e
    Signed-off-by: Xiao Lin <linxiao9292@outlook.com>

---
+++ csit/variables/alto/Variables.py
@@ -0,0 +1,7 @@
+RESOURCE_POOL_BASE = 'restconf/operational/alto-resourcepool:context'
+DEFAULT_CONTEXT_ID = '00000000-0000-0000-0000-000000000000'
+
+# ALTO IRD variables
+ALTO_SIMPLE_IRD_INFO = 'restconf/operational/alto-simple-ird:information'
+ALTO_CONFIG_IRD_INSTANCE_CONFIG = 'restconf/config/alto-simple-ird:ird-instance-configuration'
+ALTO_OPERATIONAL_IRD_INSTANCE = 'restconf/operational/alto-simple-ird:ird-instance'
