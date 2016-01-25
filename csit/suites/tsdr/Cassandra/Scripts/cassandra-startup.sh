fef1a898b0be660ad9e338b9011b1b13ec5c0302
premnath <prem.annauniv@gmail.com>
2016-01-25 14:59:22 +0530

    Added Scripts for Openflow Validation in Cassandra DB Modified TsdrUtils to fix some minor comments
    Change-Id: If6d20009c45cea16561b4c8fb7be7b1dff90ce4b
    Signed-off-by: premnath <prem.annauniv@gmail.com>

---
+++ csit/suites/tsdr/Cassandra/Scripts/cassandra-startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Installation of Cassandra
+cat > ${WORKSPACE}/cassandrastartup.sh <<EOF
+cd /tmp/cassandra
+echo "Start the Cassandra Server"
+export JAVA_HOME=/usr
+/tmp/cassandra/apache-cassandra-2.1.12/bin/cassandra
+
+EOF
+echo "Copy the Cassanra startup script to ${ODL_SYSTEM_IP}"
+scp ${WORKSPACE}/cassandrastartup.sh ${ODL_SYSTEM_IP}:/tmp
+ssh ${ODL_SYSTEM_IP} 'bash /tmp/cassandrastartup.sh'
+# vim: ts=4 sw=4 sts=4 et ft=sh :
