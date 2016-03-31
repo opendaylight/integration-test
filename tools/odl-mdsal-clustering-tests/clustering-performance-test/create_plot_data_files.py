93b48652e5c3a6dc4581e88794ec6b92a6d9d106
Luis Gomez <ecelgp@gmail.com>
2016-03-30 20:35:40 -0700

    Add WUKS and stats collection time in OpenFlow perf test.
    Change-Id: Ied3f5cffac7686c891fe0991aafa299eff3920e9
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- tools/odl-mdsal-clustering-tests/clustering-performance-test/create_plot_data_files.py
+++ tools/odl-mdsal-clustering-tests/clustering-performance-test/create_plot_data_files.py
@@ -5,17 +5,30 @@ text_file = open("out.log.txt", "r")
 log = text_file.read()
 text_file.close()
 
-data = []
+rate = []
+time = []
 
-pat = re.compile(r'Avg. requests/s: (?P<rate1>[0-9,\.]+) OK, (?P<rate2>[0-9,\.]+) Total')
+pat_rate = re.compile(r'Avg. requests/s: (?P<rate1>[0-9,\.]+) OK, (?P<rate2>[0-9,\.]+) Total')
+pat_time = re.compile(r'Stats collected in (?P<time1>[0-9,\.]+) seconds')
 
 for line in log.splitlines():
-    res = pat.search(line)
+    res = pat_rate.search(line)
     if res is not None:
-        data.append(res.groups('rate1')[0])
-print data
+        rate.append(res.groups('rate1')[0])
+print rate
+
+for line in log.splitlines():
+    res = pat_time.search(line)
+    if res is not None:
+        time.append(res.groups('time1')[0])
+print time
 
 text_file = open("rates.csv", "w")
 text_file.write('Add,Delete\n')
-text_file.write('{0},{1}\n'.format(data[0], data[1]))
+text_file.write('{0},{1}\n'.format(rate[0], rate[1]))
+text_file.close()
+
+text_file = open("times.csv", "w")
+text_file.write('Add,Delete\n')
+text_file.write('{0},{1}\n'.format(time[0], time[1]))
 text_file.close()
