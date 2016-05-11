0baeda51f4a016631af952faf3b75bc9f985677f
Peter Gubka <pgubka@cisco.com>
2016-05-11 15:21:17 +0200

    new functional suite for bgpcap using exabgp
    -tests ipv4-l3vpn
    
    Change-Id: I597728ed28cf13baba356a0f4b9dd32ffb3eefe9
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

---
+++ csit/libraries/ExaClient.py
@@ -0,0 +1,47 @@
+'''
+The purpose of this library is communicate with exabgp using rpcs.
+The server part is implemented in test/csit/scripts/bgp_rem_rpc.py.
+'''
+
+import xmlrpclib
+
+class ExaClient(object):
+
+    def __init__(self, exa_addr):
+        self.proxy = xmlrpclib.ServerProxy("http://{}:8000".format(exa_addr))
+
+    def announce(self, full_exabgp_cmd):
+        data = self.proxy.execute(full_exabgp_cmd)
+        return
+
+    def _get_counter(self, msg_type):
+        cnt = self.proxy.get_counter(msg_type)
+        return cnt
+
+    def get_received_open_count(self):
+        return self._get_counter('open')
+
+    def get_received_keepalive_count(self):
+        return self._get_counter('keepalive')
+
+    def get_received_update_count(self):
+        return self._get_counter('update')
+
+    def get_received_route_refresh_count(self):
+        return self._get_counter('route_refresh')
+
+    def _clean_counter(self, msg_type):
+        cnt = self.proxy.clean_counter(msg_type)
+
+    def clean_received_open_count(self):
+        return self._clean_counter('open')
+
+    def clean_received_keepalive_count(self):
+        return self._clean_counter('keepalive')
+
+    def clean_received_update_count(self):
+        return self._clean_counter('update')
+
+    def clean_received_route_refresh_count(self):
+        return self._clean_counter('route_refresh')
+
