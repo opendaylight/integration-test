d278c7a53038e477bfbd65fa3c52484cae8cd5d6
Kumar Rishabh <shailrishabh@gmail.com>
2016-07-09 15:56:23 +0530

    Add openflowplugin-scalability.txt scriptplan
    Also modifies Inventory_Scalability_OF13 test to test the longevity
    framework.
    
    Change-Id: I482e7981bee1cf4a16283d316c969582dc8f6f61
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

---
+++ csit/scripts/longevity_short.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Setup short duration config to ${ODL_SYSTEM_IP}"
+
+cat > ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg <<EOF
+period=5000
+
+EOF
+
+echo "Copying config files to ODL Controller folder"
+
+ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
+
+scp ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/
