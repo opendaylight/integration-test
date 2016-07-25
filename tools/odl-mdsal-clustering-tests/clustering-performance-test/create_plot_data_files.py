dd95fb5ac286fb5040cf527edd74cdf2b684b420
Luis Gomez <ecelgp@gmail.com>
2016-07-24 21:10:44 -0700

    Refactor OpenFlow NB perf test
    - Use odl_tester.py script.
    - Add fpr=200 to stress the plugin.
    
    Change-Id: I2402165cadce35c2ba0b41ca970132df86e054dc
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- tools/odl-mdsal-clustering-tests/clustering-performance-test/create_plot_data_files.py
+++ tools/odl-mdsal-clustering-tests/clustering-performance-test/create_plot_data_files.py
@@ -8,27 +8,27 @@ text_file.close()
 rate = []
 time = []
 
-pat_rate = re.compile(r'Avg. requests/s: (?P<rate1>[0-9,\.]+) OK, (?P<rate2>[0-9,\.]+) Total')
-pat_time = re.compile(r'Stats collected in (?P<time1>[0-9,\.]+) seconds')
+add_rate = re.compile(r'Add Rate: (?P<rate1>[0-9,\.]+) flows/second')
+collect_time = re.compile(r'monitoring finished in \+(?P<time1>[0-9,\.]+) seconds')
 
 for line in log.splitlines():
-    res = pat_rate.search(line)
+    res = add_rate.search(line)
     if res is not None:
         rate.append(res.groups('rate1')[0])
 print rate
 
 for line in log.splitlines():
-    res = pat_time.search(line)
+    res = collect_time.search(line)
     if res is not None:
         time.append(res.groups('time1')[0])
 print time
 
 text_file = open("rates.csv", "w")
-text_file.write('Add,Delete\n')
-text_file.write('{0},{1}\n'.format(rate[0], rate[1]))
+text_file.write('Add Rate\n')
+text_file.write('{0}\n'.format(rate[0]))
 text_file.close()
 
 text_file = open("times.csv", "w")
-text_file.write('Add,Delete\n')
+text_file.write('Add Confirm Time,Add Delete Time\n')
 text_file.write('{0},{1}\n'.format(time[0], time[1]))
 text_file.close()
