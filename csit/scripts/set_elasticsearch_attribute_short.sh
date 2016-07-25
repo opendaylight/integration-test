99fb6f122197f5cbc318ab25a611364776af9c80
Kumar Rishabh <shailrishabh@gmail.com>
2016-07-25 11:03:25 +0530

    Add support for plotting graphs in cluster
    Change-Id: Ifed0b7cdafaf10c5758f839d27c284a63a728e11
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

--- csit/scripts/set_elasticsearch_attribute_short.sh
+++ csit/scripts/set_elasticsearch_attribute_short.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Setup short duration config to ${ODL_SYSTEM_IP}"
 
 cat > ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg <<EOF
 period=5000
@@ -8,6 +7,10 @@ EOF
 
 echo "Copying config files to ODL Controller folder"
 
-ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
-
-scp ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
+        echo "Setup long duration config to ${!CONTROLLERIP}"
+        ssh ${!CONTROLLERIP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
+        scp ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg ${!CONTROLLERIP}:/tmp/${BUNDLEFOLDER}/etc/
+done
--- csit/suites/integration/Create_JVM_Plots.robot
+++ csit/suites/integration/Create_JVM_Plots.robot
@@ -1,7 +1,10 @@
 *** Settings ***
 Documentation     Test suite to Plot JVM Resources
 Resource          ${CURDIR}/../../libraries/CheckJVMResource.robot
+Resource          ${CURDIR}/../../libraries/ClusterManagement.robot
 
 *** Test Cases ***
 Create JVM Plots
-    CheckJVMResource.Create JVM Plots
+    ${is_cluster_env} =    Get Variable Value    \${ClusterManagement__has_setup_run}    False
+    BuiltIn.Run_Keyword_If    ${is_cluster_env}     CheckJVMResource.Create JVM Plots Cluster
+    ...     ELSE        CheckJVMResource.Create JVM Plots
