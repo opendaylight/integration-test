17b83b32a12b9e46004623c57baa3e881fa6429d
Kumar Rishabh <shailrishabh@gmail.com>
2016-07-25 11:03:25 +0530

    Add support for plotting graphs in cluster
    Change-Id: Ifed0b7cdafaf10c5758f839d27c284a63a728e11
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

--- csit/scripts/set_elasticsearch_attribute_long.sh
+++ csit/scripts/set_elasticsearch_attribute_long.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Setup long duration config to ${ODL_SYSTEM_IP}"
+
 
 cat > ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg <<EOF
 period=120000
@@ -7,7 +7,12 @@ period=120000
 EOF
 
 echo "Copying config files to ODL Controller folder"
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
 
-ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
+        echo "Setup long duration config to ${!CONTROLLERIP}"
+        ssh ${!CONTROLLERIP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
 
-scp ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/
+        scp ${WORKSPACE}/org.apache.karaf.decanter.scheduler.simple.cfg ${!CONTROLLERIP}:/tmp/${BUNDLEFOLDER}/etc/
+done
