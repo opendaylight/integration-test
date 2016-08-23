67965989704015bbf441712201a77f25cedc3dc8
Jamo Luhrsen <jluhrsen@redhat.com>
2016-08-23 16:43:41 -0700

    add script to toggle security group mode
    Change-Id: I74ff0cea47eab74a417f050dadda538474fabcff
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

---
+++ csit/scripts/set_sg_mode.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+cat > ${WORKSPACE}/set_sg_mode.sh <<EOF
+
+    mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/datastore/initial/config/
+    cp \$(find /tmp/${BUNDLEFOLDER} -name "*aclservice*config.xml") /tmp/${BUNDLEFOLDER}/etc/opendaylight/datastore/initial/config/
+    sed -i s/stateful/${SECURITY_GROUP_MODE}/ /tmp/${BUNDLEFOLDER}/etc/opendaylight/datastore/initial/config/netvirt-aclservice-config.xml
+    cat /tmp/${BUNDLEFOLDER}/etc/opendaylight/datastore/initial/config/netvirt-aclservice-config.xml
+
+EOF
+
+echo "Copying config files to ODL Controller folder"
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
+
+        echo "Setting security group mode to ${SECURITY_GROUP_MODE} on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/set_sg_mode.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash /tmp/set_sg_mode.sh'
+
+done
