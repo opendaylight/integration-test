15709e95877d435c603c0b56658efb44fe4c3ced
Kumar Rishabh <shailrishabh@gmail.com>
2016-08-27 02:29:18 +0530

    Cleanup Elasticsearch indices before test
    Change-Id: I9b74c2c780e535afa7ee65db8ee48f24da136f43
    
    Change-Id: I028fbe76782f17be1b0c426ce1169bf075ce6f13
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

--- csit/scripts/set_jvm_common_attribute.sh
+++ csit/scripts/set_jvm_common_attribute.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cat > ${WORKSPACE}/elasticsearch_startup.sh <<EOF
-cd /tmp/elasticsearch
-echo "Starting Elasticsearch node"
-sudo /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch > /dev/null 2>&1 &
-ls -al /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
-
-EOF
-
 cat > ${WORKSPACE}/org.apache.karaf.decanter.collector.jmx-local.cfg <<EOF
 type=jmx-local
 url=local
@@ -26,19 +18,35 @@ EOF
 for i in `seq 1 ${NUM_ODL_SYSTEM}`
 do
     CONTROLLERIP=ODL_SYSTEM_${i}_IP
+    CLUSTERNAME=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`
 
     cat > ${WORKSPACE}/elasticsearch.yml <<EOF
-    cluster.name: elasticsearch
+    cluster.name: ${CLUSTERNAME}
     network.host: ${!CONTROLLERIP}
 
 EOF
     cat > ${WORKSPACE}/org.apache.karaf.decanter.appender.elasticsearch.cfg <<EOF
     host=${!CONTROLLERIP}
     port=9300
-    clusterName=elasticsearch
+    clusterName=${CLUSTERNAME}
 
 EOF
 
+    cat > ${WORKSPACE}/elasticsearch_startup.sh <<EOF
+    cd /tmp/elasticsearch
+
+    if [ -d "data" ]; then
+        echo "data directory exists, deleting...."
+        rm -r data
+    else
+        echo "data directory does not exist"
+    fi
+
+    echo "Starting Elasticsearch node"
+    sudo /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch > /dev/null 2>&1 &
+    ls -al /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
+
+EOF
     echo "Setup ODL_SYSTEM_IP specific config files for ${!CONTROLLERIP} "
 
     cat ${WORKSPACE}/org.apache.karaf.decanter.appender.elasticsearch.cfg
@@ -52,6 +60,9 @@ EOF
     scp ${WORKSPACE}/org.apache.karaf.decanter.collector.jmx-others.cfg ${!CONTROLLERIP}:/tmp/${BUNDLEFOLDER}/etc/
 
     scp ${WORKSPACE}/elasticsearch.yml ${!CONTROLLERIP}:/tmp/
+
+    ssh ${!CONTROLLERIP} "sudo ls -al /tmp/elasticsearch/"
+
     ssh ${!CONTROLLERIP} "sudo mv /tmp/elasticsearch.yml /tmp/elasticsearch/elasticsearch-1.7.5/config/"
     ssh ${!CONTROLLERIP} "cat /tmp/elasticsearch/elasticsearch-1.7.5/config/elasticsearch.yml"
 
@@ -60,5 +71,4 @@ EOF
     scp ${WORKSPACE}/elasticsearch_startup.sh ${!CONTROLLERIP}:/tmp
     ssh ${!CONTROLLERIP} 'bash /tmp/elasticsearch_startup.sh'
     ssh ${!CONTROLLERIP} 'ps aux | grep elasticsearch'
-
 done
