da0a5e669139fdbef255511883a91ae6f064f39d
Kumar Rishabh <shailrishabh@gmail.com>
2016-08-27 02:29:18 +0530

    Cleanup Elasticsearch indices before test
    Change-Id: I9b74c2c780e535afa7ee65db8ee48f24da136f43
    
    Change-Id: I028fbe76782f17be1b0c426ce1169bf075ce6f13
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

--- csit/scripts/set_jvm_common_attribute.sh
+++ csit/scripts/set_jvm_common_attribute.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cat > ${WORKSPACE}/elasticsearch_startup.sh <<EOF
-cd /tmp/elasticsearch
-echo "Starting Elasticsearch node"
-sudo /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch > /dev/null 2>&1 &
-ls -al /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
-
-EOF
-
 cat > ${WORKSPACE}/org.apache.karaf.decanter.collector.jmx-local.cfg <<EOF
 type=jmx-local
 url=local
@@ -39,6 +31,67 @@ EOF
 
 EOF
 
+    out='{"acknowledged":true}'
+    cmd=http://${!CONTROLLERIP}:9200/_all
+    command_='curl -XDELETE http://${!CONTROLLERIP}:9200/_all 2> /dev/null'
+    command2='curl '${!CONTROLLERIP}':9200/_cat/indices?v'
+    
+    command3='curl '${!CONTROLLERIP}':9200/_cat/indices?v'
+
+    cat > ${WORKSPACE}/elasticsearch_startup.sh <<EOF
+    cd /tmp/elasticsearch
+    echo "Starting Elasticsearch node"
+    sudo /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch > /dev/null 2>&1 &
+    ls -al /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
+
+    for ((i=1;i<=100;i++));
+    do
+        output=\`curl -XDELETE http://${!CONTROLLERIP}:9200/_all > command_outputi.log\`
+        fileout=\`cat command_output.log\`
+        echo \$fileout
+        echo \${fileout}
+
+
+
+        output1=\${command2};
+        echo \$output1;
+        echo \$output;
+        echo $output;
+        echo $output1;
+        echo ${output};
+        echo ${output1};
+        echo \${output};
+        echo \${output1};
+        
+        echo \$command3;
+        echo \${command3};
+        outputX=\$command3;
+        echo \$outputX;
+        echo \${outputX};
+        echo $outputX;
+        if [[ "{\"acknowledged\":true}" ==  "\${output}" ]];
+        then
+            echo "indices deleted1";
+            break;
+        fi
+
+        if [[ "\${out}" ==  "\${output}" ]];
+        then
+            echo "indices deleted2";
+            break;
+        fi
+
+        if [[ "{\"acknowledged\":true}" ==  "\${output}" ]];
+        then
+            echo "indices deleted3";
+            break;
+        fi
+
+        echo "could not reach server, retrying";
+        sleep 2;
+    done;
+
+EOF
     echo "Setup ODL_SYSTEM_IP specific config files for ${!CONTROLLERIP} "
 
     cat ${WORKSPACE}/org.apache.karaf.decanter.appender.elasticsearch.cfg
@@ -58,7 +111,6 @@ EOF
     echo "Copying the elasticsearch_startup script to ${!CONTROLLERIP}"
     cat ${WORKSPACE}/elasticsearch_startup.sh
     scp ${WORKSPACE}/elasticsearch_startup.sh ${!CONTROLLERIP}:/tmp
-    ssh ${!CONTROLLERIP} 'bash /tmp/elasticsearch_startup.sh'
+    ssh ${!CONTROLLERIP} 'bash /tmp/elasticsearch_startup.sh' > a.log
     ssh ${!CONTROLLERIP} 'ps aux | grep elasticsearch'
-
 done
