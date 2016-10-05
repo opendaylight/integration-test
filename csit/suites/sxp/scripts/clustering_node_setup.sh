7b6f0998308745db1c68f07bde28a9170f191064
Martin Mihálek <mamihale@cisco.com>
2016-10-05 22:11:24 +0200

    WIP SXP clustering CSIT
    Change-Id: Iac83ba00c569a4c8089977a222fb7768fb5c40be
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

---
+++ csit/suites/sxp/scripts/clustering_node_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Download and start Karaf in ${TOOLS_SYSTEM_IP}"
+echo "Extracting the new controller..."
+ssh ${TOOLS_SYSTEM_IP} wget --progress=dot:mega ${ACTUALBUNDLEURL} -P /tmp/
+ssh ${TOOLS_SYSTEM_IP} unzip -q /tmp/${BUNDLE}
+echo "Configuring the startup features..."
+#TODO
+echo "Starting controller..."
+ssh ${TOOLS_SYSTEM_IP} ls /tmp/
+ssh ${TOOLS_SYSTEM_IP} ls /tmp/${BUNDLEFOLDER}
+ssh ${TOOLS_SYSTEM_IP} ls /tmp/${BUNDLEFOLDER}/bin/
+ssh ${TOOLS_SYSTEM_IP} /tmp/${BUNDLEFOLDER}/bin/start
