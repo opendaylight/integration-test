6efce9c5dfc905f109d58868556c735cb5ab5a66
Martin Mihálek <mamihale@cisco.com>
2016-10-05 22:11:24 +0200

    WIP SXP clustering CSIT
    Change-Id: Iac83ba00c569a4c8089977a222fb7768fb5c40be
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

---
+++ csit/suites/sxp/scripts/clustering_node_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Download and start Karaf in ${TOOLS_SYSTEM_IP}"
+echo "Extracting the new controller..."
+ssh ${TOOLS_SYSTEM_IP} "cd /tmp && \
+                        wget --progress=dot:mega  ${ACTUALBUNDLEURL} && \
+                        unzip -q /tmp/${BUNDLE}"
+
+echo "Configuring the startup features..."
+ssh ${TOOLS_SYSTEM_IP} "sed -ie \"s/featuresBoot=.*/featuresBoot=config,standard,region,package,kar,ssh,management,${ACTUALFEATURES}/g\" ${FEATURESCONF}"
+ssh ${TOOLS_SYSTEM_IP} "sed -ie \"s%mvn:org.opendaylight.integration/features-integration-index/${BUNDLEVERSION}/xml/features%mvn:org.opendaylight.integration/features-integration-index/${BUNDLEVERSION}/xml/features,mvn:org.opendaylight.integration/features-integration-test/${BUNDLEVERSION}/xml/features,mvn:org.apache.karaf.decanter/apache-karaf-decanter/1.0.0/xml/features%g\" ${FEATURESCONF}"
+
+echo "Starting controller..."
+ssh ${TOOLS_SYSTEM_IP} "/tmp/${BUNDLEFOLDER}/bin/start"
