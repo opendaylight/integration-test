74b7a8149df848364b7f95ecd7a38a7e67866a6f
Matej Perina <mperina@cisco.com>
2016-11-16 14:22:20 +0100

    clustering test for GBP
    Change-Id: Ie0787e6da2f8e605f84bed1b637ebdffdbe1e2fd
    Signed-off-by: Matej Perina <mperina@cisco.com>

---
+++ csit/suites/groupbasedpolicy/GBPVPP/vpp-demo/vagrant_scripts/configure-vpp.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#configure vpp
+if ! grep -q "dev 0000:00:09.0 {vlan-strip-offload off}" /etc/vpp/startup.conf; then
+    echo "configuring VPP..."
+    sudo sed -i "\$adpdk \n{\n  dev 0000:00:09.0 {vlan-strip-offload off}\n  dev 0000:00:0a.0 {vlan-strip-offload off}\n}" "/etc/vpp/startup.conf"
+    echo "configuring VPP done."
+else 
+    echo "WARNING: VPP startup conf was not configured..."
+    exit
+fi
+
+#sudo service vpp start
