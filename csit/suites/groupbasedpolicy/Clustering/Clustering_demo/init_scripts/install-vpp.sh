4f3216379a94ab683b92473e7b394964a2f061f7
Matej Perina <mperina@cisco.com>
2016-11-16 14:22:20 +0100

    clustering test for GBP
    Change-Id: Ie0787e6da2f8e605f84bed1b637ebdffdbe1e2fd
    Signed-off-by: Matej Perina <mperina@cisco.com>

---
+++ csit/suites/groupbasedpolicy/Clustering/Clustering_demo/init_scripts/install-vpp.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#install vpp
+echo "Installing VPP..."
+sysctl -w vm.nr_hugepages=1024
+HUGEPAGES=`sysctl -n  vm.nr_hugepages`
+if [ $HUGEPAGES != 1024 ]; then
+    echo "!!!!!!!!!!!!ERROR: Unable to get 1024 hugepages, only got $HUGEPAGES.  Cannot finish!!!!!!!!!!!!"
+    exit
+fi
+sudo yum -y --nogpgcheck install vpp-lib vpp vpp-devel
+echo "Installing VPP done."
