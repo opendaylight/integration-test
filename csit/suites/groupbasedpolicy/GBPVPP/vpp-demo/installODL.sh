923b5dbccfb61ea98352379e838afcaa920fec98
Matej Perina <mperina@cisco.com>
2016-11-16 14:22:20 +0100

    clustering test for GBP
    Change-Id: Ie0787e6da2f8e605f84bed1b637ebdffdbe1e2fd
    Signed-off-by: Matej Perina <mperina@cisco.com>

---
+++ csit/suites/groupbasedpolicy/GBPVPP/vpp-demo/installODL.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+sudo rm -rf /opt/distribution-karaf-0.5.0-Boron.tar.gz
+sudo rm -rf /opt/distribution-karaf-0.5.0-Boron
+if [ ! -f /vagrant/distribution-karaf-0.5.0-Boron.tar.gz ];then
+    cd /tmp/
+    echo "downloading ODL (this could take long) ..."
+    wget https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.5.0-Boron/distribution-karaf-0.5.0-Boron.tar.gz -q
+    sudo cp distribution-karaf-0.5.0-Boron.tar.gz /opt/distribution-karaf-0.5.0-Boron.tar.gz
+else
+    sudo cp /vagrant/distribution-karaf-0.5.0-Boron.tar.gz /opt/distribution-karaf-0.5.0-Boron.tar.gz
+fi
+cd /opt/
+echo "extracting ODL ..."
+sudo tar -zxf distribution-karaf-0.5.0-Boron.tar.gz
+echo "configuring ODL ..."
+sudo sed -i 's/Property name="jetty.port" default="8181"/Property name="jetty.port" default="8081"/g' /opt/distribution-karaf-0.5.0-Boron/etc/jetty.xml
+sudo sed -i 's/Property name="jetty.port" default="8080"/Property name="jetty.port" default="8182"/g' /opt/distribution-karaf-0.5.0-Boron/etc/jetty.xml
