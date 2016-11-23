24b1b1d2aef4adbdadffcadce75110213920fb81
Martin Mihálek <mamihale@cisco.com>
2016-11-23 12:15:12 +0100

    Remove Lithium only resources from SXP
    Removes lithium test plans, configuration files and checks in scripts.
    
    Change-Id: Ia12a82259f5bed2f0aea154f22bea8a0652fe57d
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

--- csit/suites/sxp/scripts/basic_node_setup.sh
+++ csit/suites/sxp/scripts/basic_node_setup.sh
@@ -3,8 +3,6 @@
 echo "Setup config to ${ODL_SYSTEM_IP}"
 ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
 
-if [ "${BRANCH#*/}" == "lithium" ]; then
-    scp ${WORKSPACE}/test/csit/suites/sxp/basic/22-sxp-controller-one-node-lithium.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/22-sxp-controller-one-node.xml
-elif [ "${BRANCH#*/}" == "beryllium" ]; then
+if [ "${BRANCH#*/}" == "beryllium" ]; then
     scp ${WORKSPACE}/test/csit/suites/sxp/basic/22-sxp-controller-one-node-beryllium.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/22-sxp-controller-one-node.xml
 fi
