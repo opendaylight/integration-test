65bbb9f4b37e3975f78fe40435de1616e1dae1e7
Matej Perina <mperina@cisco.com>
2016-12-06 15:07:59 +0100

    clustering test for GBP
    Change-Id: Ie0787e6da2f8e605f84bed1b637ebdffdbe1e2fd
    Signed-off-by: Matej Perina <mperina@cisco.com>

---
+++ csit/suites/groupbasedpolicy/Clustering/Clustering_demo/init_scripts/install-hc.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#install hc
+echo "Installing Honeycomb..."
+sudo yum -y --nogpgcheck install java-1.8.0-openjdk honeycomb
+sudo sed -i 's/"restconf-port": .*,/"restconf-port": 8283,/g' /opt/honeycomb/config/honeycomb.json
+sudo sed -i 's/"127.0.0.1",/"0.0.0.0",/g' /opt/honeycomb/config/honeycomb.json
+sudo sed -i 's/INFO/TRACE/' /opt/honeycomb/config/logback.xml
+sudo cat /opt/honeycomb/config/honeycomb.json
+echo "Installing Honeycomb done."
+sudo service honeycomb start
