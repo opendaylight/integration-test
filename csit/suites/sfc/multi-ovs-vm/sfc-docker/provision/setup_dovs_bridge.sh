279272bc76867be9669dcab4a37353821849225b
Ursicio Martin <Ursicio.Javier.Martin@ericsson.com>
2016-12-20 18:17:59 +0100

    SFC - Logical SFF: Developing Full Deploy Test Case
    Change-Id: I21c0ee8b24976fa9a7003e085efaad775120de37
    Signed-off-by: Ursicio Martin <Ursicio.Javier.Martin@ericsson.com>

---
+++ csit/suites/sfc/multi-ovs-vm/sfc-docker/provision/setup_dovs_bridge.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+BRIDGE="dovsbr0"
+BRIDGE_CFG="/etc/sysconfig/network-scripts/ifcfg-${BRIDGE}"
+ETH="eth1"
+ETH_CFG="/etc/sysconfig/network-scripts/ifcfg-${ETH}"
+[ ! -e "$BRIDGE_CFG" ] || exit 0
+echo "DEVICE=${BRIDGE}" > "$BRIDGE_CFG"
+echo "TYPE=Bridge" >> "$BRIDGE_CFG"
+echo "BOOTPROTO=dhcp" >> "$BRIDGE_CFG"
+echo "ONBOOT=yes" >> "$BRIDGE_CFG"
+echo "PERSISTENT_DHCLIENT=yes" >> "$BRIDGE_CFG"
+[ -e "$ETH_CFG" ] && mv "$ETH_CFG" "${ETH_CFG}.old"
+echo "DEVICE=${ETH}" > "$ETH_CFG"
+echo "ONBOOT=yes" >> "$ETH_CFG"
+echo "TYPE=Ethernet" >> "$ETH_CFG"
+echo "BRIDGE=${BRIDGE}" >> "$ETH_CFG"
+cat $BRIDGE_CFG
+cat $ETH_CFG
+systemctl status network.service
+journalctl -xe
+systemctl restart network
+
