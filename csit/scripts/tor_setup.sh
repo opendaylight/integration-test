b34b5c0da10670f053ae77ed00d1856cc8142a57
Dhevendran Kulandaivel <dhevendran.k@tcs.com>
2017-01-04 14:52:42 +0530

    Scripts for installing 6Wind Quagga and Open vSwitch for TOR configuration
    Change-Id: I25a39298140013321d274a7cabde2b1d7af8828d
    Signed-off-by: Dhevendran Kulandaivel <dhevendran.k@tcs.com>

---
+++ csit/scripts/tor_setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cat > ${WORKSPACE}/tor_setup.sh <<EOF
+
+echo "TOR software install procedure"
+sudo add-apt-repository -y ppa:sgauthier/openvswitch-dpdk
+sudo apt-get update -y --force-yes
+sudo apt-get install -y --force-yes openvswitch-switch
+sudo apt-get install -y --force-yes openvswitch-vtep
+sudo ovs-vswitchd --version
+
+echo '---> All Python package installation should happen in virtualenv'
+sudo apt-get install -y --force-yes python-virtualenv python-pip
+
+echo '---> Install OVS 2.4 Python module'
+#wget -c https://pypi.python.org/packages/2f/8a/358cad389613865ee255c7540f9ea2c2f98376c2d9cd723f5cf30390d928/ovs-2.4.0.tar.gz#md5=9097ced87a88e67fbc3d4b92c16e6b71
+#tar -zxvf ovs-2.4.0.tar.gz
+#cd ovs-2.4.0
+#sudo mkdir -p /var/run/openvswitch/
+#sudo python setup.py install
+cd ..
+
+#Stop  OVS 2.5 services
+sudo killall -9 ovs-vtep
+sudo killall -9 ovs-vswitchd
+sudo killall -9 ovsdb-server
+ps -ef | grep ovs
+
+#Configure OVS 2.5 TOR emulation
+sudo rm /etc/openvswitch/ovs.db
+sudo rm /etc/openvswitch/vtep.db
+export OVS_HOME="/usr/share/openvswitch/"
+sudo ovsdb-tool create /etc/openvswitch/ovs.db $OVS_HOME/vswitch.ovsschema
+sudo ovsdb-tool create /etc/openvswitch/vtep.db $OVS_HOME/vtep.ovsschema
+sleep 1
+sudo ovsdb-server --pidfile --detach --log-file --remote punix:/usr/var/run/openvswitch/db.sock \
+--remote=db:hardware_vtep,Global,managers /etc/openvswitch/ovs.db /etc/openvswitch/vtep.db
+sudo ovs-vsctl --no-wait init
+sudo ovs-vswitchd --pidfile --detach
+sudo ovs-vsctl add-br br0
+sleep 1
+sudo ovs-vsctl show
+sudo vtep-ctl add-ps br0
+sudo vtep-ctl set Physical_Switch br0 tunnel_ips=12.0.0.11
+
+
+EOF
+
+echo "Execute the TOR software install procedure on all the tools VMs"
+for i in `seq 1 ${NUM_TOOLS_SYSTEM}`
+do
+        CONTROLLERIP=TOOLS_SYSTEM_${i}_IP
+        echo "Setting security group mode to ${SECURITY_GROUP_MODE} on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/tor_setup.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash /tmp/tor_setup.sh'
+done
