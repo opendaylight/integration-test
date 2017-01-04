24ce2fd1fba991ae9b22dc9a32d1054b746257f9
Dhevendran Kulandaivel <dhevendran.k@tcs.com>
2017-01-04 14:52:42 +0530

    Scripts for installing 6Wind Quagga and Open vSwitch for TOR configuration
    Change-Id: I25a39298140013321d274a7cabde2b1d7af8828d
    Signed-off-by: Dhevendran Kulandaivel <dhevendran.k@tcs.com>

---
+++ csit/scripts/tor_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat > ${WORKSPACE}/tor_setup.sh <<EOF
+
+echo "TOR software install procedure"
+add-apt-repository -y ppa:sgauthier/openvswitch-dpdk
+apt-get update -y --force-yes
+apt-get install -y --force-yes openvswitch-switch
+apt-get install -y --force-yes openvswitch-vtep
+ovs-vswitchd --version
+
+echo '---> All Python package installation should happen in virtualenv'
+apt-get install -y --force-yes python-virtualenv python-pip
+
+echo '---> Install OVS 2.4 Python module'
+wget -c https://pypi.python.org/packages/2f/8a/358cad389613865ee255c7540f9ea2c2f98376c2d9cd723f5cf30390d928/ovs-2.4.0.tar.gz#md5=9097ced87a88e67fbc3d4b92c16e6b71
+tar -zxvf ovs-2.4.0.tar.gz
+cd ovs-2.4.0
+mkdir -p /var/run/openvswitch/
+python setup.py install
+cd ..
+
+EOF
+
+echo "Execute the TOR software install procedure on all the tools VMs"
+for i in `seq 1 ${NUM_TOOLS_SYSTEM}`
+do
+        CONTROLLERIP=TOOLS_SYSTEM_${i}_IP
+        echo "Setting security group mode to ${SECURITY_GROUP_MODE} on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/tor_setup.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash sudo /tmp/tor_setup.sh'
+done
