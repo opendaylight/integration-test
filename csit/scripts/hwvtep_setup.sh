5554a5095eab4369a8e469f78361d3cad2ca91f7
abbas <abbas.pareedkunju@tcs.com>
2017-01-06 02:48:22 -0800

    HWVTEP Initialization Tests
    Change-Id: I58cd84775cad140a65c1f71d8b533d88fda4f29e
    Signed-off-by: abbas <abbas.pareedkunju@tcs.com>

---
+++ csit/scripts/hwvtep_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat > ${WORKSPACE}/hwvtep_setup.sh <<EOF
+echo "HWVTEP software install procedure"
+echo '---> Install OpenVSwitch 2.5.0'
+add-apt-repository -y ppa:sgauthier/openvswitch-dpdk
+apt-get update -y --force-yes
+apt-get install -y --force-yes openvswitch-switch openvswitch-vtep
+
+echo '---> All Python package installation should happen in virtualenv'
+apt-get install -y --force-yes python-virtualenv python-pip
+
+EOF
+
+echo "Execute the TOR software install procedure on all the tools VMs"
+for i in `seq 1 ${NUM_TOOLS_SYSTEM}`
+do
+        CONTROLLERIP=TOOLS_SYSTEM_${i}_IP
+        echo "Setting security group mode to ${SECURITY_GROUP_MODE} on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/hwvtep_setup.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'sudo bash /tmp/hwvtep_setup.sh'
+done
+
+
