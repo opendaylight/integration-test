dd8d416ba8077db67324d3ecc753ee0f65d1de87
Sandra Biton <sandra.biton@hpe.com>
2017-01-07 23:06:41 +0200

    WIP: add federation rabbit
    Change-Id: I237093bf1a2bdbeb31600abf56ca3e4645857d91
    Signed-off-by: Sandra Biton <sandra.biton@hpe.com>

---
+++ csit/scripts/set_federation_rabbit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ ${CONTROLLERFEATURES} == *federation* ]]; then
+
+    cat > ${WORKSPACE}/set_federation_rabbit.sh <<EOF
+
+        echo "Install rabbit server ..."
+        sudo yum install -y erlang
+        sudo yum install -y rabbitmq-server
+
+        echo "Starting rabbit server ..."
+        sudo chkconfig rabbitmq-server
+        sudo service rabbitmq-server start
+
+        echo "Add federation user to rabbit server ..."
+        sudo /usr/sbin/rabbitmqctl add_user federation federation
+        sudo rabbitmqctl set_permissions -p / federation ".*" ".*" ".*"
+
+        echo "Configuring rabbit broker for ODL ..."
+        SERVICECONF=\$(find "/tmp/${BUNDLEFOLDER}/" -name "federation-service-impl-*config.xml")
+        sed -ie "s/<site-ip>.*</<site-ip>\$2</g" \${SERVICECONF}
+        sed -ie "s/<mqBrokerIp>.*</<mqBrokerIp>\$1</g" \${SERVICECONF}
+        sed -ie "s/CONTROL_QUEUE_.*</CONTROL_QUEUE_\$1</g" \${SERVICECONF}
+        sed -ie "s/guest/federation/g" \${SERVICECONF}
+        cat \${SERVICECONF}
+
+    EOF
+
+    echo "Copying config files to ODL Controller folder"
+    for i in `seq 1 ${NUM_ODL_SYSTEM}`
+    do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
+        ODL_SITE_IP=${ODL_SYSTEM_1_IP}
+        if [ ${NUM_ODL_SYSTEM} -gt 1 ]; then
+            ODL_SITE_IP=${OPENSTACK_COMPUTE_NODE_3_IP}
+        fi
+        echo "Setting rabbit client site ip to ${ODL_MGR_IP} on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/set_federation_rabbit.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash /tmp/set_federation_rabbit.sh' ${!CONTROLLERIP} ${ODL_SITE_IP}
+    done
+
+fi
