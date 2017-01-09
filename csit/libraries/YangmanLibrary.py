3b3adf1daa3486de6768db24be76ee06f1c934e4
Daša Šimková <dasa.simkova@pantheon.tech>
2017-01-09 17:26:50 +0100

    Add basic Yangman robot test
    - add Yangman basic test
    - add Dlux testing keywords
    - add Yangman testing keywords
    - add Yangman gui variables
    - add Yangman testplan
    
    Change-Id: Ide1e593a972887df942a30dfbca115989961c9ee
    Signed-off-by: Daša Šimková <dasa.simkova@pantheon.tech>
    Signed-off-by: Dasa Simkova -X (dsimkova - PANTHEON TECHNOLOGIES at Cisco) <dsimkova@cisco.com>

---
+++ csit/libraries/YangmanLibrary.py
@@ -0,0 +1,16 @@
+"""
+Yangman library for additional keywords needed for testing Yangman module.
+Author: Dasa Simkova
+"""
+
+
+def Verify_String_Contains_Substring(full_string, substring):
+    """Removes leading and trailing spaces from a chosen string."""
+    if substring in full_string:
+        return True
+    else:
+        return False
+
+
+
+
