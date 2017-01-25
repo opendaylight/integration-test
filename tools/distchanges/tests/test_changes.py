fc39efb93d456de380cdece4207b480ab0c5c602
Sam Hague <shague@redhat.com>
2017-01-25 10:51:00 -0500

    Add protections around changes pretty_print
    Change-Id: I569b7044cb2255173b39916774c4a33b79812502
    Signed-off-by: Sam Hague <shague@redhat.com>
    
    tox
    
    Change-Id: I9bb90d9f88ed09630c25e9493f75bc6b62a53848
    Signed-off-by: Sam Hague <shague@redhat.com>

--- tools/distchanges/tests/test_changes.py
+++ tools/distchanges/tests/test_changes.py
@@ -19,17 +19,37 @@ class TestChanges(unittest.TestCase):
 
     @staticmethod
     def run_cmd(branch, distro_patch, limit, qlimit, project_names, remote_url):
-        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url)
+        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url, 3)
         projects = changes.run_cmd()
         changes.pretty_print_projects(projects)
 
     def test_run_cmd_single(self):
-        project_names = ['openflowplugin']
+        project_names = ['odlparent']
         self.run_cmd(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
 
     def test_run_cmd_multiple(self):
         project_names = PROJECT_NAMES
         self.run_cmd(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
+
+    def test_pretty_print(self):
+        project_names = PROJECT_NAMES
+        changes = Changes(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
+        projects = {}
+        for project in project_names:
+            projects[project] = {"commit": 1, "includes": [{'a': 1}]}
+        changes.pretty_print_projects(projects)
+        for project in project_names:
+            projects[project] = {"commit": 1,
+                                 "includes": [{"grantedOn": 1, "lastUpdated": 11,
+                                               "number": "12345", "subject": "This is a test for " + project},
+                                              {"grantedOn": 2, "lastUpdated": 22,
+                                               "number": "56789", "subject": "This is a test for " + project}]}
+        changes.pretty_print_projects(projects)
+
+    def test_epoch_to_utc(self):
+        project_names = PROJECT_NAMES
+        changes = Changes(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
+        print("utc: %s" % changes.epoch_to_utc(1483974872))
 
 
 if __name__ == '__main__':
