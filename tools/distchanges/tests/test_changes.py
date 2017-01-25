2332778db07bde044c0b236eeb808add36de907d
Sam Hague <shague@redhat.com>
2017-01-25 12:43:36 -0500

    print date in utc format rather than epoch
    Change-Id: I17218f02e71b3d6408e1c0eced3e4c1c7c9579ca
    Signed-off-by: Sam Hague <shague@redhat.com>
    
    wip
    
    Change-Id: I90a579afb4cb88f20f1efda3e144980ce01bd9e0
    Signed-off-by: Sam Hague <shague@redhat.com>

--- tools/distchanges/tests/test_changes.py
+++ tools/distchanges/tests/test_changes.py
@@ -5,7 +5,9 @@ import unittest
 from changes import Changes
 
 REMOTE_URL = 'ssh://git.opendaylight.org:29418'
-PROJECT_NAMES = ['genius', 'mdsal', 'netvirt', 'neutron', 'openflowjava', 'openflowplugin', 'ovsdb', 'yangtools']
+# PROJECT_NAMES = ['genius', 'mdsal', 'netvirt', 'neutron', 'openflowjava', 'openflowplugin', 'ovsdb', 'yangtools']
+PROJECT_NAMES = ['netconf', 'ovsdb', 'dlux', 'mdsal', 'dluxapps', 'netvirt', 'yangtools', 'openflowplugin', 'genius',
+                 'controller', 'infrautils', 'odlparent', 'sfc', 'neutron']
 DISTRO_PATH = "/tmp/distribution-karaf"
 BRANCH = 'master'
 LIMIT = 10
@@ -19,12 +21,12 @@ class TestChanges(unittest.TestCase):
 
     @staticmethod
     def run_cmd(branch, distro_patch, limit, qlimit, project_names, remote_url):
-        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url, 3)
+        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url, 0)
         projects = changes.run_cmd()
         changes.pretty_print_projects(projects)
 
     def test_run_cmd_single(self):
-        project_names = ['odlparent']
+        project_names = ['netvirt']
         self.run_cmd(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
 
     def test_run_cmd_multiple(self):
@@ -45,6 +47,11 @@ class TestChanges(unittest.TestCase):
                                               {"grantedOn": 2, "lastUpdated": 22,
                                                "number": "56789", "subject": "This is a test for " + project}]}
         changes.pretty_print_projects(projects)
+
+    def test_epoch_to_utc(self):
+        project_names = PROJECT_NAMES
+        changes = Changes(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
+        print("utc: %s" % changes.epoch_to_utc(1483974872))
 
 if __name__ == '__main__':
     unittest.main()
