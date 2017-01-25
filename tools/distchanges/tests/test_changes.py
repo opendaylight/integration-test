66a2b913393494de422a832710077ee8d84b7f91
Sam Hague <shague@redhat.com>
2017-01-25 12:43:36 -0500

    print date in utc format rather than epoch
    Change-Id: I17218f02e71b3d6408e1c0eced3e4c1c7c9579ca
    Signed-off-by: Sam Hague <shague@redhat.com>
    
    wip
    
    Change-Id: I90a579afb4cb88f20f1efda3e144980ce01bd9e0
    Signed-off-by: Sam Hague <shague@redhat.com>

--- tools/distchanges/tests/test_changes.py
+++ tools/distchanges/tests/test_changes.py
@@ -21,12 +21,12 @@ class TestChanges(unittest.TestCase):
 
     @staticmethod
     def run_cmd(branch, distro_patch, limit, qlimit, project_names, remote_url):
-        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url, 3)
+        changes = Changes(branch, distro_patch, limit, qlimit, project_names, remote_url, 0)
         projects = changes.run_cmd()
         changes.pretty_print_projects(projects)
 
     def test_run_cmd_single(self):
-        project_names = ['odlparent']
+        project_names = ['netvirt']
         self.run_cmd(BRANCH, DISTRO_PATH, LIMIT, QLIMIT, project_names, REMOTE_URL)
 
     def test_run_cmd_multiple(self):
