11e23f9f8bdff62c052d1f07935fbb609f1e7d73
rahul.agarwal <rahul.agarwal3@tcs.com>
2017-01-31 02:13:33 -0800

    DCGW test cases
    Change-Id: Ice3d015cb4024a42a86092f3bbcd0477e8206e85
    Signed-off-by: rahul.agarwal <rahul.agarwal3@tcs.com>

---
+++ csit/scripts/dcgw_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat > ${WORKSPACE}/dcgw_setup.sh <<EOF
+
+echo "DC-Gateway install procedure"
+#Create Build directory
+    rm -rf /tmp/6wind_quagga_build_dir
+    mkdir /tmp/6wind_quagga_build_dir
+    cd /tmp/6wind_quagga_build_dir
+
+#Clean the directory if exists
+    rm -rf c-capnproto thrift zeromq4-1 quagga zrpcd
+    
+echo "dowloading zrpcd"
+    git clone https://github.com/6WIND/zrpcd.git
+    cd zrpcd
+    chmod -R 777 pkgsrc/dev_compile_script.sh
+    sudo bash pkgsrc/dev_compile_script.sh -d -b -t
+    
+EOF
+
+echo "Execute the DC-Gateway install procedure on all the tools VMS"
+
+for i in `seq 1 ${NUM_TOOLS_SYSTEM}`
+do
+        CONTROLLERIP=TOOLS_SYSTEM_${i}_IP
+        echo "Copying the /tmp/dcgw_setup.sh to Tools System IP ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/dcgw_setup.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'sudo bash /tmp/dcgw_setup.sh'
+done
+
+echo "Execute the DC-Gateway install procedure on all the ODL VMS"
+
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+    CONTROLLERIP=ODL_SYSTEM_${i}_IP
+    echo "Copying the /tmp/dcgw_setup.sh to ODL System  IP ${!CONTROLLERIP}"
+    scp ${WORKSPACE}/dcgw_setup.sh ${!CONTROLLERIP}:/tmp/
+    ssh ${!CONTROLLERIP} 'sudo bash /tmp/dcgw_setup.sh'
+done
+
+
