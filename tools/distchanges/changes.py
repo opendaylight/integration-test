a48b6c6791698440ae11050665e29cbe8dceb964
Sam Hague <shague@redhat.com>
2017-02-22 20:51:00 -0500

    Add distchanges lookup for aggregated merges
    Some distributions end up with a aggregated merge
    of a number of gerrits in a patch stack. The data included in the git.properties
    includes all the gerrit's commit messages. This gerrit adds a
    search using the commit message of the patch at the top of the stack. [1]
    is an example of one of these types of gerrits.
    
    [1] https://git.opendaylight.org/gerrit/#/c/51304/
    
    Change-Id: I26663b44e08b2f09c18dfff98efe560fbeaae4e4
    Signed-off-by: Sam Hague <shague@redhat.com>

--- tools/distchanges/changes.py
+++ tools/distchanges/changes.py
@@ -215,18 +215,39 @@ class Changes(object):
         if changeid:
             return changeid.group()
 
-        # Didn"t find a Change-Id so try to get a commit message
+        # Didn't find a Change-Id so try to get a commit message
         # match on "blah" but only keep the blah
+        if self.verbose >= 2:
+            print("did not find Change-Id in %s, trying with commit-msg" % (project))
         regex_msg = re.compile(r'"([^"]*)"|^git.commit.message.short=(.*)$')
         msg = regex_msg.search(pfile)
-        if self.verbose >= 2:
-            print("did not find Change-Id in %s, trying with commit-msg: %s" % (project, msg.group()))
-
         if msg:
-            # TODO: add new query using this msg
+            if self.verbose >= 2:
+                print("did not find Change-Id in %s, trying with commit-msg: %s" % (project, msg.group()))
+
+                # TODO: add new query using this msg
             gerrits = self.gerritquery.get_gerrits(project, None, 1, msg.group())
             if gerrits:
                 return gerrits[0]["id"]
+
+        # Maybe one of the monster 'merge the world' gerrits
+        regex_msg = re.compile(r'git.commit.message.full=(.*)')
+        msg = regex_msg.search(pfile)
+        first_msg = None
+        if msg:
+            lines = str(msg.group()).split("\\n")
+            for i, line in enumerate(lines):
+                if str(line).find('* changes\\:') != -1:
+                    if i + 1 < len(lines):
+                        first_msg = lines[i+1]
+                        break
+        if first_msg:
+            gerrits = self.gerritquery.get_gerrits(project, None, 1, first_msg)
+            if gerrits:
+                return gerrits[0]["id"]
+
+        print("did not find Change-Id for %s" % project)
+
         return None
 
     def find_distro_changeid(self, project):
