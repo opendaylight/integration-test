94095d96bd6aa7fb1965d4f46fcb3ff0aa675c5c
lbeles <lbeles@frinx.io>
2017-02-24 14:25:24 +0100

    Add flow table metrics test for Elastic Search TSDR
    Change-Id: I8dc51a59e327d82f4533b4e17a9806edab7f71de
    Signed-off-by: lbeles <lbeles@frinx.io>

---
+++ csit/suites/tsdr/Elasticsearch/scripts/elasticsearch-startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installation of Elasticsearch.
+cat > ${WORKSPACE}/elasticsearch.sh <<EOF
+cd /tmp/elasticsearch
+echo "Start the Elasticsearch Server"
+export JAVA_HOME=/usr
+cd /tmp/elasticsearch/elasticsearch-1.7.5/bin
+sudo /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
+ls -l /tmp/elasticsearch/elasticsearch-1.7.5/bin/elasticsearch
+
+EOF
+
+echo "Copy the Elasticsearch startup script to ${ODL_SYSTEM_IP}"
+scp ${WORKSPACE}/elasticsearch.sh ${ODL_SYSTEM_IP}:/tmp
+ssh ${ODL_SYSTEM_IP} 'bash /tmp/elasticsearch.sh'
+ssh ${ODL_SYSTEM_IP} 'ps -ef | grep elasticsearch'
