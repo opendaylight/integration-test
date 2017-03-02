4d44c4d053e3d0289896f431bae11a9d4b1d208e
Peter Gubka <pgubka@cisco.com>
2017-03-02 17:08:05 +0100

    Add DOMNotificationBroker Test Suite
    Change-Id: I91501de578ca629ef9f3ab1f2f43b25ed496e3be
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

---
+++ csit/libraries/MdsalLowlevelPy.py
@@ -0,0 +1,54 @@
+"""
+Python invocation of several rpcs defined in module module odl-mdsal-lowlevel-control.yang.
+"""
+import Queue
+import requests
+import string
+import threading
+
+
+def publish_notifications(host, grprefix, duration, rate, nrpairs=1):
+    """Invoke publish notification rpc and verify the response
+
+    :param host: ip address of odl node
+    :type host: string
+    :param grprefix: prefix identifier for publisher/listener pair
+    :type grprefix: string
+    :param duration: publishing notification duration in seconds
+    :type duration: int
+    :param rate: events rate per second
+    :type rate: int
+    :param nrpairs: number of publisher/listener pairs, id suffix is counted from it
+    :type nrpairs: int
+    """
+    def _publ_notifications(rqueue, url, grid, duration, rate):
+        dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <notifications-per-second>$RATE</notifications-per-second>
+</input>''')
+        data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate})
+        try:
+            resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
+                                 data=data, auth=('admin', 'admin'), timeout=duration+60)
+        except Exception as exc:
+            resp = exc
+        print 'Response for{}: {}'.format(grid, resp)
+        rqueue.put(resp)
+
+    resqueue = Queue.Queue()
+    lthreads = []
+    url = 'http://{}:8181/restconf/operations/odl-mdsal-lowlevel-control:publish-notifications'.format(host)
+    for i in range(nrpairs):
+        t = threading.Thread(target=_publ_notifications,
+                             args=(resqueue, url, '{}{}'.format(grprefix, i+1), duration, rate))
+        t.daemon = True
+        t.start()
+        lthreads.append(t)
+
+    for t in lthreads:
+        t.join()
+
+    for i in range(nrpairs):
+        resp = resqueue.get()
+        assert resp.status_code == 200
