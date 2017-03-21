a77a3e9c2ad5db12b63bd33d258254dc14de0082
Peter Gubka <pgubka@cisco.com>
2017-03-21 12:32:08 +0100

    Add initial files for dom data broker testing
    Change-Id: Ib45527e7a565ab8283c0d5f2fffd023274e6e20f
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

--- csit/libraries/MdsalLowlevelPy.py
+++ csit/libraries/MdsalLowlevelPy.py
@@ -6,6 +6,7 @@ import requests
 import string
 import threading
 
+_globals = {}
 
 def publish_notifications(host, grprefix, duration, rate, nrpairs=1):
     """Invoke publish notification rpcs and verify the response.
@@ -51,3 +52,58 @@ def publish_notifications(host, grprefix, duration, rate, nrpairs=1):
     for i in range(nrpairs):
         resp = resqueue.get()
         assert resp.status_code == 200
+
+def initiate_write_transactions_on_nodes(host_list, grid, duration, rate, chained_flag=True):
+    """Invoke publish notification rpcs and verify the response.
+
+    :param host_list: list of ip address of odl nodes
+    :type host_list: list of strings
+    :param grid: identifier
+    :type grid: string
+    :param duration: time in seconds
+    :type duration: int
+    :param rate: publishing notification duration in seconds
+    :type rate: int
+    :param chained_flag: events rate per second
+    :type chained: bool
+    """
+    def _write_transactions(rqueue, url, grid, duration, rate, chained_flag):
+        dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <transactions-per-second>$RATE</transactions-per-second>
+  <chained-transactions>$CHAINED_FLAG</chained-transactions>
+</input>''')
+        data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate, 'CHAINED_FLAG': chained_flag})
+        try:
+            resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
+                                 data=data, auth=('admin', 'admin'), timeout=int(duration)+60)
+        except Exception as exc:
+            resp = exc
+        rqueue.put(resp)
+
+    resqueue = Queue.Queue()
+    lthreads = []
+    for host in range(hosts):
+        url = 'http://{}:8181/restconf/operations/odl-mdsal-lowlevel-control:write-transactions'.format(host)
+        t = threading.Thread(target=_publ_notifications, args=(resqueue, url, grid, duration, rate, chained_flag))
+        t.daemon = True
+        t.start()
+        lthreads.append(t)
+
+    _globals.update({'threads': lthreads, 'hosts': hosts, 'result_queue': resqueue})
+
+
+def wait_for_write_transactions():
+    lthreads = _globals.pop('threads')
+    hosts = _globals.pop('hosts')
+    resqueue = _globals.pop('result_queue')
+
+    for t in lthreads:
+        t.join()
+
+    results = []
+    for host in hosts:
+        results.append(resqueue.get())
+    return results
+
