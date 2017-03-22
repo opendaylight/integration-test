5a692823dfb52c58df5e467e2453fdfeb7f16105
Nazeerahamed <nazeerahamed.hunshyal@tcs.com>
2017-03-22 07:47:51 -0700

    BFD tunnel monitoring test cases suite.
    New library added and Moved Datastore dump to this
    common genius library file
    
    Adding BFD file to testplan file of genius
    Added __init__.robot to executed before BFD suite
    
    Incorporate the review comments given by JamO like
    implemented local keyword for repatative code,
    removed sleeps, and defined a array to log required
    datastore.
    
    Removed single file execution which was there in genius
    test plan file.
    
    Update of review comment of patch 18 and rename of variable
    in "Get Model Dump" keyword of common library
    
    Review update on patch 19.
    1. Add comment line for converting hex to decimal
    2. Delete unwanted logs
    3. Have a keyword for repeat lines
    
    Change-Id: I0b6cff1c53b725c8f852ef0b063e37a73fce761a
    Signed-off-by: Nazeerahamed <nazeerahamed.hunshyal@tcs.com>

---
+++ csit/variables/genius/Modules.py
@@ -0,0 +1,16 @@
+bfd_data_models = [
+        'config/itm-config:tunnel-monitor-enabled',
+        'config/itm-config:tunnel-monitor-interval',
+        'config/itm-state:dpn-endpoints',
+        'config/itm-state:external-tunnel-list',
+        'config/itm:transport-zones',
+        'config/network-topology:network-topology',
+        'config/opendaylight-inventory:nodes',
+        'operational/ietf-interfaces:interfaces',
+        'operational/ietf-interfaces:interfaces-state',
+        'operational/itm-config:tunnel-monitor-enabled',
+        'operational/itm-config:tunnel-monitor-interval',
+        'operational/itm-state:tunnels_state',
+        'operational/network-topology:network-topology',
+        'operational/odl-interface-meta:bridge-ref-info',
+]
