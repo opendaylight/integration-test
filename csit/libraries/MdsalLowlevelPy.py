3522dd431a42aa0df27e4aca0b7e61b79e2ec2e7
Peter Gubka <pgubka@cisco.com>
2017-04-20 15:14:58 +0200

    Add prefix based shard dom-data-broker suites
    Change-Id: I54326b1aefa5e9beacaab5b8a418d9ab93a9c7e7
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

--- csit/libraries/MdsalLowlevelPy.py
+++ csit/libraries/MdsalLowlevelPy.py
@@ -11,45 +11,42 @@ import threading
 _globals = {}
 
 
-def start_write_transactions_on_nodes(host_list, id_prefix, duration, rate, chained_flag=True):
-    """Invoke publish notification rpcs and verify the response.
+def _send_http_request_thread_impl(rqueue, url, data):
+    """Start either publish or write transactions rpc based on input.
+
+    :param rqueue: result queue
+    :type rqueue: Queue.Queue
+    :param url: rpc url
+    :type url: string
+    :param data: http request content
+    :type data: string
+    """
+    logger.info('rpc indoked with details: {}'.format(data))
+    try:
+        resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
+                             data=data, auth=('admin', 'admin'), timeout=int(duration)+60)
+    except Exception as exc:
+        resp = exc
+        logger.debug(exc)
+    rqueue.put(resp)
+
+
+def _initiate_rpcs(host_list, url_templ, data)
+    """Initiate rpc on given hosts.
 
     :param host_list: list of ip address of odl nodes
     :type host_list: list of strings
-    :param id_prefix: identifier prefix
-    :type id_prefix: string
-    :param duration: time in seconds
-    :type duration: int
-    :param rate: publishing notification duration in seconds
-    :type rate: int
-    :param chained_flag: events rate per second
-    :type chained: bool
+    :param url_templ: url template
+    :type url_templ: string.Template object
+    :param data: http request data
+    :type data: string
     """
-    def _write_transactions(rqueue, url, grid, duration, rate, chained_flag):
-        dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
-  <id>$ID</id>
-  <seconds>$DURATION</seconds>
-  <transactions-per-second>$RATE</transactions-per-second>
-  <chained-transactions>$CHAINED_FLAG</chained-transactions>
-</input>''')
-        data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate, 'CHAINED_FLAG': chained_flag})
-        logger.info('write-transactions rpc indoked with details: {}'.format(data))
-        try:
-            resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
-                                 data=data, auth=('admin', 'admin'), timeout=int(duration)+60)
-        except Exception as exc:
-            resp = exc
-            logger.debug(exc)
-        rqueue.put(resp)
-
-    logger.info("Input parameters: host_list:{}, id_prefix:{}, duration:{}, rate:{}, chained_flag:{}".format(
-        host_list, id_prefix, duration, rate, chained_flag))
     resqueue = _globals.pop('result_queue', Queue.Queue())
     lthreads = _globals.pop('threads', [])
     for i, host in enumerate(host_list):
-        url = 'http://{}:8181/restconf/operations/odl-mdsal-lowlevel-control:write-transactions'.format(host)
-        t = threading.Thread(target=_write_transactions,
-                             args=(resqueue, url, '{}{}'.format(id_prefix, i), duration, rate, chained_flag))
+        url = url_templ.substitute({'HOST': host})
+        t = threading.Thread(target=_send_http_request_thread_impl,
+                             args=(resqueue, url, data))
         t.daemon = True
         t.start()
         lthreads.append(t)
@@ -57,7 +54,59 @@ def start_write_transactions_on_nodes(host_list, id_prefix, duration, rate, chai
     _globals.update({'threads': lthreads, 'result_queue': resqueue})
 
 
-def wait_for_write_transactions():
+def start_write_transactions_on_nodes(host_list, id_prefix, duration, rate, chained_flag=True):
+    """Invoke write-transactions rpc on given nodes.
+
+    :param host_list: list of ip address of odl nodes
+    :type host_list: list of strings
+    :param id_prefix: identifier prefix
+    :type id_prefix: string
+    :param duration: time in seconds
+    :type duration: int
+    :param rate: write transactions rate in transactions per second
+    :type rate: int
+    :param chained_flag: chained or simple transactions flag
+    :type chained: bool
+    """
+    logger.info("Input parameters: host_list:{}, id_prefix:{}, duration:{}, rate:{}, chained_flag:{}".format(
+        host_list, id_prefix, duration, rate, chained_flag))
+    dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <transactions-per-second>$RATE</transactions-per-second>
+  <chained-transactions>$CHAINED_FLAG</chained-transactions>
+</input>''')
+    data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate, 'CHAINED_FLAG': chained_flag})
+    _initiate_rpcs(host_list, string.Template('''http://$HOST:8181/restconf/operations/odl-mdsal-lowlevel-control:write-transactions'''), data)
+
+
+def start_produce_transactions_on_nodes(host_list, id_prefix, duration, rate, isolated_transactions_flag=True):
+    """Invoke produce-transactions rpcs on given nodes.
+
+    :param host_list: list of ip address of odl nodes
+    :type host_list: list of strings
+    :param id_prefix: identifier prefix
+    :type id_prefix: string
+    :param duration: time in seconds
+    :type duration: int
+    :param rate: produce transactions rate in transactions per second
+    :type rate: int
+    :param isolated_transactions_flag: isolated transactions flag
+    :type isolated_transactions_flag: bool
+    """
+    logger.info("Input parameters: host_list:{}, id_prefix:{}, duration:{}, rate:{}, isolated_transactions:{}".format(
+        host_list, id_prefix, duration, rate, isolated_transactions_flag))
+    dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <transactions-per-second>$RATE</transactions-per-second>
+  <isolated-transactions>$ISOLATED_TRANSACTIONS</isolated-transactions>
+</input>''')
+    data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate, 'ISOLATED_TRANSACTIONS': isolated_transactions_flag})
+    _initiate_rpcs(host_list, string.Template('''http://$HOST:8181/restconf/operations/odl-mdsal-lowlevel-control:produce-transactions'''), data)
+
+
+def wait_for_transactions():
     """Blocking call, waitig for responses from all threads"""
     lthreads = _globals.pop('threads')
     resqueue = _globals.pop('result_queue')
@@ -78,16 +127,3 @@ def get_next_write_transactions_response():
     if not resqueue.empty():
         return resqueue.get()
     return None
-
-
-def finish_write_transactions():
-    lthreads = _globals.pop('threads')
-    resqueue = _globals.pop('result_queue')
-
-    for t in lthreads:
-        t.join()
-
-    results = []
-    while not resqueue.empty():
-        results.append(resqueue.get())
-    return results
---
+++ csit/suites/controller/dom_data_broker/leader_isolation_prefbasedshard.robot
@@ -0,0 +1,32 @@
+*** Settings ***
+Documentation     DOMDataBroker testing: Leader Isolation
+...
+...               Copyright (c) 2017 Cisco Systems, Inc. and others. All rights reserved.
+...
+...               This program and the accompanying materials are made available under the
+...               terms of the Eclipse Public License v1.0 which accompanies this distribution,
+...               and is available at http://www.eclipse.org/legal/epl-v10.html
+...
+...               The goal is to ensure the datastore succeeds in basic isolation/rejoin scenario,
+...               simulating either a network partition, or a prolonged GC pause.
+Suite Setup       BuiltIn.Run_Keywords    SetupUtils.Setup_Utils_For_Setup_And_Teardown
+                  ...    AND    DdbCommons.Create_Prefix_Based_Shard
+Suite Teardown    BuiltIn.Run_Keywords    DdbCommons.Remove_Prefix_Based_Shard
+                  ...    AND    SSHLibrary.Close_All_Connections
+Test Setup        SetupUtils.Setup_Test_With_Logging_And_Without_Fast_Failing
+Test Teardown     SetupUtils.Teardown_Test_Show_Bugs_If_Test_Failed
+Default Tags      critical
+Test Template     DdbCommons.Leader_Isolation_PrefBasedShard_Test_Templ
+Library           SSHLibrary
+Resource          ${CURDIR}/../../../libraries/controller/DdbCommons.robot
+Resource          ${CURDIR}/../../../libraries/SetupUtils.robot
+
+*** Test Cases ***
+Healing_Within_Transaction_Timeout
+    [Documentation]    The isolated node (leader) is rejoined as soon as new leader is elected and
+    ...    and within transaction timeout.
+    ${0}
+
+Healing_After_2x_Transaction_Timeout
+    [Documentation]    The isolated node (leader) is rejoined after 2x transaction timeout.
+    ${${TRANSACTION_TIMEOUT_2X}+1}
