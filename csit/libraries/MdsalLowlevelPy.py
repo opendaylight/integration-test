e63fca318c5b3b84e36f96a3945933974363716d
Peter Gubka <pgubka@cisco.com>
2017-04-20 15:14:58 +0200

    Add prefix based shard dom-data-broker suites
    Change-Id: I54326b1aefa5e9beacaab5b8a418d9ab93a9c7e7
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

--- csit/libraries/MdsalLowlevelPy.py
+++ csit/libraries/MdsalLowlevelPy.py
@@ -11,45 +11,52 @@ import threading
 _globals = {}
 
 
-def start_write_transactions_on_nodes(host_list, id_prefix, duration, rate, chained_flag=True):
-    """Invoke publish notification rpcs and verify the response.
+def _send_http_request_thread_impl(rqueue, url, data, http_timeout):
+    """Start either publish or write transactions rpc based on input.
+
+    :param rqueue: result queue
+    :type rqueue: Queue.Queue
+    :param url: rpc url
+    :type url: string
+    :param data: http request content
+    :type data: string
+    :param http_timeout: http response timeout
+    :type http_timeout: int
+    """
+    logger.info('rpc indoked with details: {}'.format(data))
+    try:
+        resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
+                             data=data, auth=('admin', 'admin'), timeout=http_timeout)
+    except Exception as exc:
+        resp = exc
+        logger.debug(exc)
+    rqueue.put(resp)
+
+
+def _initiate_rpcs(host_list, prefix_list, url_templ, data_templ, subst_dict):
+    """Initiate rpc on given hosts.
 
     :param host_list: list of ip address of odl nodes
     :type host_list: list of strings
-    :param id_prefix: identifier prefix
+    :param id_prefix: list of node indexes which coresponds to the ip addresses
     :type id_prefix: string
-    :param duration: time in seconds
-    :type duration: int
-    :param rate: writing transactions rate per second
-    :type rate: int
-    :param chained_flag: specify chained vs. simple transactions
-    :type chained_flag: bool
+    :param url_templ: url template
+    :type url_templ: string.Template object
+    :param data_templ: http request data
+    :type data_templ: string.Template object
+    :param subst_dict: dictionary with key value pairs to be used with template
+    :type subst_dict: dict
     """
-    def _write_transactions(rqueue, url, grid, duration, rate, chained_flag):
-        dtmpl = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
-  <id>$ID</id>
-  <seconds>$DURATION</seconds>
-  <transactions-per-second>$RATE</transactions-per-second>
-  <chained-transactions>$CHAINED_FLAG</chained-transactions>
-</input>''')
-        data = dtmpl.substitute({'ID': grid, 'DURATION': duration, 'RATE': rate, 'CHAINED_FLAG': chained_flag})
-        logger.info('write-transactions rpc indoked with details: {}'.format(data))
-        try:
-            resp = requests.post(url=url, headers={'Content-Type': 'application/xml'},
-                                 data=data, auth=('admin', 'admin'), timeout=int(duration)+60)
-        except Exception as exc:
-            resp = exc
-            logger.debug(exc)
-        rqueue.put(resp)
-
-    logger.info("Input parameters: host_list:{}, id_prefix:{}, duration:{}, rate:{}, chained_flag:{}".format(
-        host_list, id_prefix, duration, rate, chained_flag))
     resqueue = _globals.pop('result_queue', Queue.Queue())
     lthreads = _globals.pop('threads', [])
     for i, host in enumerate(host_list):
-        url = 'http://{}:8181/restconf/operations/odl-mdsal-lowlevel-control:write-transactions'.format(host)
-        t = threading.Thread(target=_write_transactions,
-                             args=(resqueue, url, '{}{}'.format(id_prefix, i), duration, rate, chained_flag))
+        subst_dict.update({'ID': '{}{}'.format(subst_dict['ID_PREFIX'], prefix_list[i])})
+        url = url_templ.substitute({'HOST': host})
+        data = data_templ.substitute(subst_dict)
+        timeout = int(subst_dict['DURATION'])+60
+        logger.info('url: {}, data: {}, timeout: {}'.format(url, data, timeout))
+        t = threading.Thread(target=_send_http_request_thread_impl,
+                             args=(resqueue, url, data, timeout))
         t.daemon = True
         t.start()
         lthreads.append(t)
@@ -57,8 +64,65 @@ def start_write_transactions_on_nodes(host_list, id_prefix, duration, rate, chai
     _globals.update({'threads': lthreads, 'result_queue': resqueue})
 
 
-def wait_for_write_transactions():
-    """Blocking call, waiting for responses from all threads."""
+def start_write_transactions_on_nodes(host_list, prefix_list, id_prefix, duration, rate, chained_flag=False):
+    """Invoke write-transactions rpc on given nodes.
+
+    :param host_list: list of ip address of odl nodes
+    :type host_list: list of strings
+    :param id_prefix: list of node indexes which coresponds to the ip addresses
+    :type id_prefix: string
+    :param id_prefix: identifier prefix
+    :type id_prefix: string
+    :param duration: time in seconds
+    :type duration: int
+    :param rate: writing transactions rate in transactions per second
+    :type rate: int
+    :param chained_flag: specify chained vs. simple transactions
+    :type chained_flag: bool
+    """
+    logger.info("Input parameters: host_list:{}, prefix_list:{}, id_prefix:{}, duration:{}, rate:{}, chained_flag:{}".format(
+        host_list, prefix_list, id_prefix, duration, rate, chained_flag))
+    datat = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <transactions-per-second>$RATE</transactions-per-second>
+  <chained-transactions>$CHAINED_FLAG</chained-transactions>
+</input>''')
+    subst_dict = {'ID_PREFIX': id_prefix, 'DURATION': duration, 'RATE': rate, 'CHAINED_FLAG': chained_flag}
+    urlt = string.Template('''http://$HOST:8181/restconf/operations/odl-mdsal-lowlevel-control:write-transactions''')
+    _initiate_rpcs(host_list, prefix_list, urlt, datat, subst_dict)
+
+
+def start_produce_transactions_on_nodes(host_list, prefix_list, id_prefix, duration, rate, isolated_transactions_flag=True):
+    """Invoke produce-transactions rpcs on given nodes.
+
+    :param host_list: list of ip address of odl nodes
+    :type host_list: list of strings
+    :param id_prefix: identifier prefix
+    :type id_prefix: string
+    :param duration: time in seconds
+    :type duration: int
+    :param rate: produce transactions rate in transactions per second
+    :type rate: int
+    :param isolated_transactions_flag: isolated transactions flag
+    :type isolated_transactions_flag: bool
+    """
+    logger.info("Input parameters: host_list:{}, id_prefix:{}, duration:{}, rate:{}, isolated_transactions:{}".format(
+        host_list, id_prefix, duration, rate, isolated_transactions_flag))
+    datat = string.Template('''<input xmlns="tag:opendaylight.org,2017:controller:yang:lowlevel:control">
+  <id>$ID</id>
+  <seconds>$DURATION</seconds>
+  <transactions-per-second>$RATE</transactions-per-second>
+  <isolated-transactions>$ISOLATED_TRANSACTIONS</isolated-transactions>
+</input>''')
+    subst_dict = {'ID_PREFIX': id_prefix, 'DURATION': duration, 'RATE': rate,
+                  'ISOLATED_TRANSACTIONS': isolated_transactions_flag}
+    urlt = string.Template('''http://$HOST:8181/restconf/operations/odl-mdsal-lowlevel-control:produce-transactions''')
+    _initiate_rpcs(host_list, urlt, datat, subst_dict)
+
+
+def wait_for_transactions():
+    """Blocking call, waitig for responses from all threads."""
     lthreads = _globals.pop('threads')
     resqueue = _globals.pop('result_queue')
 
@@ -72,7 +136,7 @@ def wait_for_write_transactions():
     return results
 
 
-def get_next_write_transactions_response():
+def get_next_transactions_response():
     """Get http response from write-transactions rpc if available."""
     resqueue = _globals.get('result_queue')
 
---
+++ csit/suites/controller/dom_data_broker/remote_listener_prefbasedshard.robot
@@ -0,0 +1,32 @@
+*** Settings ***
+Documentation     DOMDataBroker testing: Remote Listener
+...
+...               Copyright (c) 2017 Cisco Systems, Inc. and others. All rights reserved.
+...
+...               This program and the accompanying materials are made available under the
+...               terms of the Eclipse Public License v1.0 which accompanies this distribution,
+...               and is available at http://www.eclipse.org/legal/epl-v10.html
+...
+...               The goal is to ensure listeners do no observe disruption when the leader moves.
+...               This is performed by having a steady stream of transactions being observed by
+...               the listeners and having the leader move.
+Suite Setup       BuiltIn.Run_Keywords    SetupUtils.Setup_Utils_For_Setup_And_Teardown
+...               AND    DdbCommons.Create_Prefix_Based_Shard
+Suite Teardown    BuiltIn.Run_Keywords    DdbCommons.Remove_Prefix_Based_Shard
+...               AND    SSHLibrary.Close_All_Connections
+Test Setup        SetupUtils.Setup_Test_With_Logging_And_Without_Fast_Failing
+Test Teardown     SetupUtils.Teardown_Test_Show_Bugs_If_Test_Failed
+Default Tags      critical
+Test Template     DdbCommons.Remote_Listener_PrefBasedShard_Test_Templ
+Library           SSHLibrary
+Resource          ${CURDIR}/../../../libraries/controller/DdbCommons.robot
+Resource          ${CURDIR}/../../../libraries/SetupUtils.robot
+
+*** Test Cases ***
+Listener_On_Shard_Leader_Node
+    [Documentation]    Listener runs on leader node when leader changed.
+    leader
+
+Listener_On_Shard_Non_Leader_Node
+    [Documentation]    Listener runs on non-leader node when leader changed.
+    non-leader
