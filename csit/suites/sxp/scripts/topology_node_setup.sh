53529a3b27333cdcbce597983f74c07510f4ada5
Martin Mihálek <mamihale@cisco.com>
2017-05-12 13:53:31 +0200

    Remove SXP beryllium resources
    Change-Id: I260d7eccf66343be7d33e986e1fd3428b9c91b90
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

--- csit/suites/sxp/scripts/topology_node_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Setup config to ${ODL_SYSTEM_IP}"
-ssh ${ODL_SYSTEM_IP} "mkdir -p /tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/"
-
-if [ "${BRANCH#*/}" == "beryllium" ]; then
-    scp ${WORKSPACE}/test/csit/suites/sxp/topology/22-sxp-controller-one-node-beryllium.xml ${ODL_SYSTEM_IP}:/tmp/${BUNDLEFOLDER}/etc/opendaylight/karaf/22-sxp-controller-one-node.xml
-fi
