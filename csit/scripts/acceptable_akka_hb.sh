44805ad59fac23b35a13c7c060d1ef888fc0565f
Peter Gubka <pgubka@cisco.com>
2017-05-12 20:41:27 +0200

    Add acceptable akka hb variable
    Change-Id: I536da3c8fe91cc84d81d026ebb9ce586ccb48ccc
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

---
+++ csit/scripts/acceptable_akka_hb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+AKKAHBPAUSE=5
+AKKAFACTORY=/tmp/${BUNDLEFOLDER}/system/org/opendaylight/controller/sal-clustering-config/*/sal-clustering-config-*-factoryakkaconf.xml
+
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+    CONTROLLERIP=ODL_SYSTEM_${i}_IP
+    echo "Set akka acceptable-heartbeat-pause"
+    ssh ${!CONTROLLERIP} "sed -ie 's/failure-detector.acceptable-heartbeat-pause = .*/failure-detector.acceptable-heartbeat-pause = ${AKKAHBPAUSE} s/g' ${AKKAFACTORY}"
+    ssh ${!CONTROLLERIP} "cat ${AKKAFACTORY}"
+done
