5c440d97862f50fecbdbf04c95bc9d94c8b00424
gvrangan <venkatrangang@hcl.com>
2017-05-23 03:22:44 +0530

    Adding Additional Debug Information for VTN Jobs
    Change-Id: I6eb8ccf8920c8b8ab29f90553208de3efd505095
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

--- csit/variables/vtn/Modules.py
+++ csit/variables/vtn/Modules.py
@@ -1,7 +1,9 @@
 vtn_data_models = [
         'config/network-topology:network-topology/topology/ovsdb:1',
         'config/opendaylight-inventory:nodes',
-        'operational/opendaylight-inventory:nodes',
+        'operational/opendaylight-inventory:nodes/node/openflow:1/table/0',
+        'operational/opendaylight-inventory:nodes/node/openflow:2/table/0',
+        'operational/opendaylight-inventory:nodes/node/openflow:3/table/0',
         'operational/entity-owners:entity-owners',
         'operational/vtn-inventory:vtn-nodes',
         'operational/network-topology:network-topology/topology/ovsdb:1'
