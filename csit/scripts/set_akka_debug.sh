6b11cbc91eb5b7f3fec162b7ca10aa4ac8cbb2d5
Luis Gomez <ecelgp@gmail.com>
2017-11-01 01:58:20 -0700

    Modify seed-nodes-timeout in OF cluster test
    Change-Id: Ib74cc9d4c77fc9978eb1df248e1c1d4070068724
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- csit/scripts/set_akka_debug.sh
+++ csit/scripts/set_akka_debug.sh
@@ -4,6 +4,7 @@ cat > ${WORKSPACE}/set_akka_debug.sh <<EOF
 
   echo "Enable AKKA debug"
   sed -i -e 's/akka {/akka {\nloglevel = "DEBUG"\nactor {\ndebug {\nautoreceive = on\nlifecycle = on\nunhandled = on\nfsm = on\nevent-stream = on\n}\n}/' ${AKKACONF}
+  sed -i -e 's/cluster {/cluster {\nseed-node-timeout = 6s\n/' ${AKKACONF}
   echo "Dump ${AKKACONF}"
   cat ${AKKACONF}
   echo "log4j.logger.akka.remote=DEBUG" >> ${LOGCONF}
