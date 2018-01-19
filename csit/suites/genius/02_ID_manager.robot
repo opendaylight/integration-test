901b4895991f49a11af672f67f49c116457e24f4
Faseela K <faseela.k@ericsson.com>
2018-01-19 13:27:42 +0530

    Enhance genius CSIT Teardown dumps
    The teardown dumps were missing some of the critical
    REST URLs for debugging
    
    Change-Id: I80a0f539db9aefb7cf3dba4398e01e08a855741d
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- csit/suites/genius/02_ID_manager.robot
+++ csit/suites/genius/02_ID_manager.robot
@@ -2,7 +2,7 @@
 Documentation     Test Suite for Interface manager
 Suite Setup       Create Session    session    http://${ODL_SYSTEM_IP}:${RESTCONFPORT}    auth=${AUTH}    headers=${HEADERS}
 Suite Teardown    Delete All Sessions
-Test Teardown     Get Model Dump    ${ODL_SYSTEM_IP}    ${bfd_data_models}
+Test Teardown     Get Model Dump    ${ODL_SYSTEM_IP}    ${idmanager_data_models}
 Library           OperatingSystem
 Library           String
 Library           RequestsLibrary
--- csit/variables/genius/Modules.py
+++ csit/variables/genius/Modules.py
@@ -1,16 +1,30 @@
-bfd_data_models = [
+data_models = [
+    'config/ietf-interfaces:interfaces',
+    'config/interface-service-bindings:service-bindings',
+    'config/itm:transport-zones',
     'config/itm-config:tunnel-monitor-enabled',
     'config/itm-config:tunnel-monitor-interval',
+    'config/itm-config:tunnel-monitor-params',
     'config/itm-state:dpn-endpoints',
     'config/itm-state:external-tunnel-list',
-    'config/itm:transport-zones',
     'config/network-topology:network-topology',
+    'config/odl-interface-meta:bridge-interface-info',
+    'config/odl-interface-meta:interface-child-info',
     'config/opendaylight-inventory:nodes',
-    'operational/ietf-interfaces:interfaces',
     'operational/ietf-interfaces:interfaces-state',
+    'operational/interface-service-bindings:bound-services-state-list',
     'operational/itm-config:tunnel-monitor-enabled',
     'operational/itm-config:tunnel-monitor-interval',
+    'operational/itm-config:tunnel-monitor-params',
     'operational/itm-state:tunnels_state',
     'operational/network-topology:network-topology',
     'operational/odl-interface-meta:bridge-ref-info',
+    'operational/odl-interface-meta:dpn-to-interface-list',
+    'operational/odl-interface-meta:if-indexes-interface-map',
+    'operational/opendaylight-inventory:nodes',
+]
+
+idmanager_data_models = [
+    'config/id-manager:id-pools/',
+    'operational/lock-manager:locks/',
 ]
