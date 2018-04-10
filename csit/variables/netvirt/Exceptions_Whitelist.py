98b6a4241659b7c3ffa6090fafd0f88b5c066e4b
Jamo Luhrsen <jluhrsen@redhat.com>
2018-04-10 14:28:47 -0700

    Remove ':' OLFE exception in whitelist
    seems that we sometimes get OptimisticLockFailedExceptions
    with and without the ':' on the end. Remove it for now so
    we can still ignore OLFEs in CSIT
    
    Change-Id: Iab56968dff0e6dbc8840f05fc986edc089e5479b
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- csit/variables/netvirt/Exceptions_Whitelist.py
+++ csit/variables/netvirt/Exceptions_Whitelist.py
@@ -1,5 +1,5 @@
 NETVIRT_EXCEPTIONS_WHITELIST = [
-    'OptimisticLockFailedException:',
+    'OptimisticLockFailedException',
     'ConflictingModificationAppliedException: Node was created by other transaction',
     'ConflictingModificationAppliedException: Node was deleted by other transaction',
     'org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry msgType: 1 oxm_field: 33 experimenterID: null was not found - please verify that all needed deserializers ale loaded correctly' # noqa
