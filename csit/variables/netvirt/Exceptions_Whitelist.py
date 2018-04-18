9cbd7e4204ccf52024223c0282ad94f81cc401e7
Jamo Luhrsen <jluhrsen@redhat.com>
2018-04-18 14:44:13 -0700

    Do not fail on ODLPARENT-152
    this is a special case exception we want to ignore and
    not cause failures in the robot test cases.
    
    The Exception prints a generic 2nd line that was falling
    through our normal whitelist process. So this special case
    will be caught IFF those two lines appear back to back.
    
    Change-Id: I65b437af3e53a7474fa64ef1b9e42e52e35cf778
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- csit/variables/netvirt/Exceptions_Whitelist.py
+++ csit/variables/netvirt/Exceptions_Whitelist.py
@@ -5,3 +5,5 @@ NETVIRT_EXCEPTIONS_WHITELIST = [
     'org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry msgType: 1 oxm_field: 33 experimenterID: null was not found - please verify that all needed deserializers ale loaded correctly', # noqa
     'InterruptedByTimeoutException: null'
 ]
+
+ODLPARENT_152 = ['ERROR | Karaf ssh console user karaf | ShellUtil', 'java.lang.NullPointerException: null']
