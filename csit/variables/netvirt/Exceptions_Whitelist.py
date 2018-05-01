c8ccfa6f2370bd0ffb37213b5995b098c741c9d8
Sam Hague <shague@redhat.com>
2018-04-30 20:17:13 -0400

    Add exception for NEUTRON-157
    This job hit NEUTRON-157 and a new exception is coming out
    from an mdsal change in the api. The job below has an example.
    
    https://logs.opendaylight.org/releng/vex-yul-odl-jenkins-1/netvirt-csit-1node-openstack-queens-gate-stateful-fluorine/281/odl_1/odl1_karaf.log.gz
    
    Change-Id: I7c5a7c6f3dbb8013d217168b09d3ce9a0213978d
    Signed-off-by: Sam Hague <shague@redhat.com>

--- csit/variables/netvirt/Exceptions_Whitelist.py
+++ csit/variables/netvirt/Exceptions_Whitelist.py
@@ -2,6 +2,7 @@ NETVIRT_EXCEPTIONS_WHITELIST = [
     'OptimisticLockFailedException',
     'ConflictingModificationAppliedException: Node was created by other transaction',
     'ConflictingModificationAppliedException: Node was deleted by other transaction',
+    'org.opendaylight.mdsal.common.api.MappingCheckedFuture.wrapInExecutionException',
     'org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry msgType: 1 oxm_field: 33 experimenterID: null was not found - please verify that all needed deserializers ale loaded correctly', # noqa
     'InterruptedByTimeoutException: null'
 ]
