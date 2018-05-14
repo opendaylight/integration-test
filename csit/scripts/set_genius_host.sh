623009d10328429e4a271adcbbc6ab3d8b48c57f
Sathwik Boggarapu <b.sathwik@ericsson.com>
2018-05-14 12:04:23 +0530

    Enhance Genius-CSIT to 3 Switches
    WIP:Adding one more Switch to existing genius-csit.
    
    Change-Id: Ieb8800ff52f14264267fddf6ca61e1f2bab60dc0
    Signed-off-by: Sathwik Boggarapu <b.sathwik@ericsson.com>

---
+++ csit/scripts/set_genius_host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat > ${WORKSPACE}/system-ovs-restart.sh <<EOF
+
+sudo rm -rf /etc/openvswitch/conf.db 
+sudo service openvswitch-switch restart
+
+EOF
+scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_2_IP}:/tmp/
+ssh ${TOOLS_SYSTEM_2_IP} 'sudo bash /tmp/system-ovs-restart.sh'
+scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_3_IP}:/tmp/
+ssh ${TOOLS_SYSTEM_3_IP} 'sudo bash /tmp/system-ovs-restart.sh'
