e4afdafd12a061b07aab58fcc8aaf50f2c266e18
Taseer Ahmed <taseer94@gmail.com>
2018-06-13 23:18:35 +0500

    Add minimal unit tests
    - Modularize push_to_elk script
    - Setup environment variables
    - Add dummy data under testfiles
    
    Change-Id: I49a3f61a6a9632f53f8819ef24fd12163036e170
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- csit/scripts/push_to_elk.py
+++ csit/scripts/push_to_elk.py
@@ -12,10 +12,8 @@
 ##############################################################################
 
 """
-This script is used to parse logs, construct JSON BODY and push
+This script is used to parse logs, construct JSON body and push
 it to ELK DB.
-
-Usage: python construct_json.py host:port
 
 JSON body similar to following is constructed from robot files, jenkins environment
 and plot files available in workspace available post-build.
@@ -50,98 +48,94 @@ from datetime import datetime
 import glob
 import json
 import os
-import requests
 import sys
 import time
 import xml.etree.ElementTree as ET
 
 # 3rd party lib
+import requests
 import yaml
 
-# ELK DB host and port to be passed as ':' separated argument
-
-if len(sys.argv) > 1:
-    if ':' in sys.argv[1]:
-        ELK_DB_HOST = sys.argv[1].split(':')[0]
-        ELK_DB_PORT = sys.argv[1].split(':')[1]
-else:
-    print('Usage: python push_to_elk.py host:port')
-    print('Unable to publish data to ELK. Exiting.')
-    sys.exit()
 
 # Construct json body
+def construct_json():
+    """
+    This function gathers all the data in JSON format.
+    """
+    body = {}
 
-BODY = {}
+    ts = time.time()
+    formatted_ts = \
+        datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    body['@timestamp'] = formatted_ts
 
-ts = time.time()
-formatted_ts = \
-    datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-BODY['@timestamp'] = formatted_ts
+    # Plots are obtained from csv files (present in archives directory in $WORKSPACE).
 
-# Plots are obtained from csv files (present in archives directory in $WORKSPACE).
+    csv_files = glob.glob('archives/*.csv')
+    body['project'] = 'opendaylight'
+    body['subject'] = 'test'
 
-csv_files = glob.glob('archives/*.csv')
-BODY['project'] = 'opendaylight'
-BODY['subject'] = 'test'
+    # If there are no csv files, then it is a functional test.
+    # Parse csv files and fill perfomance parameter values
 
-# If there are no csv files, then it is a functional test.
-# Parse csv files and fill perfomance parameter values
+    if len(csv_files) == 0:
+        body['test-type'] = 'functional'
+    else:
+        body['test-type'] = 'performance'
+        body['plots'] = {}
+        for f in csv_files:
+            key = (f.split('/')[-1])[:-4]
+            body['plots'][key] = {}
+            with open(f) as file:
+                lines = file.readlines()
+            props = lines[0].strip().split(',')
+            vals = lines[1].strip().split(',')
+            body['plots'][key][props[0]] = float(vals[0])
+            body['plots'][key][props[1]] = float(vals[1])
+            body['plots'][key][props[2]] = float(vals[2])
 
-if len(csv_files) == 0:
-    BODY['test-type'] = 'functional'
-else:
-    BODY['test-type'] = 'performance'
-    BODY['plots'] = {}
-    for f in csv_files:
-        key = (f.split('/')[-1])[:-4]
-        BODY['plots'][key] = {}
-        with open(f) as file:
-            lines = file.readlines()
-        props = lines[0].strip().split(',')
-        vals = lines[1].strip().split(',')
-        BODY['plots'][key][props[0]] = float(vals[0])
-        BODY['plots'][key][props[1]] = float(vals[1])
-        BODY['plots'][key][props[2]] = float(vals[2])
+    # Fill the required parameters whose values are obtained from environment.
+    body['jenkins-silo'] = os.environ['SILO']
+    body['test-name'] = os.environ['JOB_NAME']
+    body['test-run'] = os.environ['BUILD_NUMBER']
 
-# Fill the required parameters whose values are obtained from environment.
+    # Parsing robot log for statistics on no of start-time, pass/fail tests and duration.
+    robot_log = glob.glob('{}/output.*'.format(os.environ['WORKSPACE']))
+    tree = ET.parse(robot_log[0])
+    body['id'] = '{}-{}'.format(os.environ['JOB_NAME'],
+                                os.environ['BUILD_NUMBER'])
+    body['start-time'] = tree.getroot().attrib['generated']
+    body['pass-tests'] = tree.getroot().find('statistics')[0][1].get('pass')
+    body['fail-tests'] = tree.getroot().find('statistics')[0][1].get('fail')
+    endtime = tree.getroot().find('suite').find('status').get('endtime')
+    starttime = tree.getroot().find('suite').find('status').get('starttime')
+    elap_time = datetime.strptime(endtime, '%Y%m%d %H:%M:%S.%f') \
+        - datetime.strptime(starttime, '%Y%m%d %H:%M:%S.%f')
+    body['duration'] = str(elap_time)
 
-BODY['jenkins-silo'] = os.environ['SILO']
-BODY['test-name'] = os.environ['JOB_NAME']
-BODY['test-run'] = os.environ['BUILD_NUMBER']
+    return body
 
-# Parsing robot log for statistics on no of start-time, pass/fail tests and duration.
 
-robot_log = os.environ['WORKSPACE'] + '/output.xml'
-tree = ET.parse(robot_log)
-BODY['id'] = '{}-{}'.format(os.environ['JOB_NAME'],
-                            os.environ['BUILD_NUMBER'])
-BODY['start-time'] = tree.getroot().attrib['generated']
-BODY['pass-tests'] = tree.getroot().find('statistics')[0][1].get('pass')
-BODY['fail-tests'] = tree.getroot().find('statistics')[0][1].get('fail')
-endtime = tree.getroot().find('suite').find('status').get('endtime')
-starttime = tree.getroot().find('suite').find('status').get('starttime')
-elap_time = datetime.strptime(endtime, '%Y%m%d %H:%M:%S.%f') \
-    - datetime.strptime(starttime, '%Y%m%d %H:%M:%S.%f')
-BODY['duration'] = str(elap_time)
+def push_to_elastic(body, ELK_DB_HOST, ELK_DB_PORT):
+    """
+    Parse JSON body to construct PUT_URL.
+    """
 
-# Parse JSON BODY to construct PUT_URL
+    PUT_URL_INDEX = '/{}-{}'.format(body['project'], body['subject'])
+    PUT_URL_TYPE = '/{}'.format(body['test-type'])
+    PUT_URL_ID = '/{}-{}'.format(body['test-name'], body['test-run'])
+    PUT_URL = 'https://{}:{}{}{}{}'.format(ELK_DB_HOST, ELK_DB_PORT, PUT_URL_INDEX, PUT_URL_TYPE, PUT_URL_ID)
+    print(PUT_URL)
 
-PUT_URL_INDEX = '/{}-{}'.format(BODY['project'], BODY['subject'])
-PUT_URL_TYPE = '/{}'.format(BODY['test-type'])
-PUT_URL_ID = '/{}-{}'.format(BODY['test-name'], BODY['test-run'])
-PUT_URL = 'https://{}:{}{}{}{}'.format(ELK_DB_HOST, ELK_DB_PORT, PUT_URL_INDEX, PUT_URL_TYPE, PUT_URL_ID)
-print(PUT_URL)
+    print(json.dumps(body, indent=4))
 
-print(json.dumps(BODY, indent=4))
-
-# Try to send request to ELK DB.
-
-try:
-    r = requests.put(PUT_URL, json=BODY)
-    print(r.status_code)
-    print(json.dumps(r.json(), indent=4))
-except:
-    print('Unable to push data to ElasticSearch')
+    # Try to send request to ELK DB.
+    try:
+        r = requests.put(PUT_URL, json=body)
+        print(r.status_code)
+        print(json.dumps(r.json(), indent=4))
+    except:
+        print('Unable to push data to ElasticSearch')
 
 
 # Function to convert JSON object to string.
@@ -302,100 +296,100 @@ def getVisualization(testname, fieldlist, plotkey=''):
     return vis
 
 
-vis_ids = []
-if BODY['test-type'] == 'performance':
+def publish_dashboard(body, ELK_DB_HOST, ELK_DB_PORT):
+    vis_ids = []
+    if body['test-type'] == 'performance':
 
-    # Create visualizations for performance tests
-    # One visualization for one plot
+        # Create visualizations for performance tests
+        # One visualization for one plot
 
-    for key in BODY['plots']:
-        fieldlist = []
-        for subkey in BODY['plots'][key]:
-            fieldlist.append('plots.' + key + '.' + subkey)
-        vis = getVisualization(BODY['test-name'], fieldlist, key)
-        vis_ids.append(BODY['test-name'] + '-' + key)
-        PUT_URL = \
-            'https://{}:{}/.kibana/visualization/{}-{}'.format(ELK_DB_HOST, ELK_DB_PORT, BODY['test-name'], key)
-        print(PUT_URL)
-        print(json.dumps(vis, indent=4))
-        try:
-            r = requests.put(PUT_URL, json=vis)
-            print(r.status_code)
-            print(json.dumps(r.json(), indent=4))
-        except:
-            print('Unable to push visualization to Kibana')
+        for key in body['plots']:
+            fieldlist = []
+            for subkey in body['plots'][key]:
+                fieldlist.append('plots.' + key + '.' + subkey)
+            vis = getVisualization(body['test-name'], fieldlist, key)
+            vis_ids.append(body['test-name'] + '-' + key)
+            PUT_URL = \
+                'https://{}:{}/.kibana/visualization/{}-{}'.format(ELK_DB_HOST, ELK_DB_PORT, body['test-name'], key)
+            print(PUT_URL)
+            print(json.dumps(vis, indent=4))
+            try:
+                r = requests.put(PUT_URL, json=vis)
+                print(r.status_code)
+                print(json.dumps(r.json(), indent=4))
+            except:
+                print('Unable to push visualization to Kibana')
 
-vis = getVisualization(BODY['test-name'],
-                       ['pass-tests', 'failed-tests'])
-vis_ids.append(BODY['test-name'])
-PUT_URL = 'https://{}:{}/.kibana/visualization/{}'.format(ELK_DB_HOST, ELK_DB_PORT, BODY['test-name'])
-print(PUT_URL)
-print(json.dumps(vis, indent=4))
-try:
-    r = requests.put(PUT_URL, json=vis)
+    vis = getVisualization(body['test-name'], ['pass-tests', 'failed-tests'])
+    vis_ids.append(body['test-name'])
+    PUT_URL = 'https://{}:{}/.kibana/visualization/{}'.format(ELK_DB_HOST, ELK_DB_PORT, body['test-name'])
+    print(PUT_URL)
+    print(json.dumps(vis, indent=4))
+    try:
+        r = requests.put(PUT_URL, json=vis)
+        print(r.status_code)
+        print(json.dumps(r.json(), indent=4))
+    except:
+        print('Unable to push dashboard to Kibana')
+
+    # Create dashboard and add above created visualizations to it
+
+    DASHBOARD_NAME = body['test-name'].split('-')[0]
+    dashboard = {}
+    dashboard['title'] = DASHBOARD_NAME
+    dashboard['description'] = 'Dashboard for visualizing ' \
+        + DASHBOARD_NAME
+    dashboard['uiStateJSON'] = '{}'
+    dashboard['optionsJSON'] = '{"darkTheme":false}'
+    dashboard['version'] = 1
+    dashboard['timeRestore'] = False
+    dashboard['kibanaSavedObjectMeta'] = {
+        'searchSourceJSON': '{"filter":[{"query":{"query_string":{"query":"*","analyze_wildcard":true}}}],'
+        '"highlightAll":true,"version":true}'
+    }
+
+    # Check if visualizations already present in dashboard. If present, don't add, else, add at end
+
+    GET_URL = 'https://{}:{}/.kibana/dashboard/{}'.format(ELK_DB_HOST, ELK_DB_PORT, DASHBOARD_NAME)
+    r = requests.get(GET_URL)
+    response = r.json()
+    dashboard_found = response['found']
+    vis_ids_present = set()
+    panelsJSON = []
+
+    print(json.dumps(response, indent=4))
+    if dashboard_found:
+        panelsJSON = yaml.safe_load(response['_source']['panelsJSON'])
+        for vis in panelsJSON:
+            vis_ids_present.add(vis['id'])
+
+    size_x = 6
+    size_y = 3
+    xpos = (len(vis_ids_present) % 2) * 6 + 1
+    ypos = (len(vis_ids_present) / 2) * 3 + 1
+    for (i, vis_id) in enumerate(vis_ids):
+        if (not dashboard_found or vis_id not in vis_ids_present):
+            panelJSON = {
+                'size_x': size_x,
+                'size_y': size_y,
+                'panelIndex': len(vis_ids_present) + i,
+                'type': 'visualization',
+                'id': vis_id,
+                'col': xpos,
+                'row': ypos,
+            }
+            xpos += size_x
+            if xpos > 12:
+                xpos = 1
+                ypos += size_y
+            panelsJSON.append(panelJSON)
+        else:
+            print('visualization ' + vis_id + ' already present in dashboard')
+
+    dashboard['panelsJSON'] = JSONToString(panelsJSON)
+    PUT_URL = 'https://{}:{}/.kibana/dashboard/{}'.format(ELK_DB_HOST, ELK_DB_PORT, DASHBOARD_NAME)
+    print(PUT_URL)
+    print(json.dumps(dashboard, indent=4))
+    r = requests.put(PUT_URL, json=dashboard)
     print(r.status_code)
     print(json.dumps(r.json(), indent=4))
-except:
-    print('Unable to push dashboard to Kibana')
-
-# Create dashboard and add above created visualizations to it
-
-DASHBOARD_NAME = BODY['test-name'].split('-')[0]
-dashboard = {}
-dashboard['title'] = DASHBOARD_NAME
-dashboard['description'] = 'Dashboard for visualizing ' \
-    + DASHBOARD_NAME
-dashboard['uiStateJSON'] = '{}'
-dashboard['optionsJSON'] = '{"darkTheme":false}'
-dashboard['version'] = 1
-dashboard['timeRestore'] = False
-dashboard['kibanaSavedObjectMeta'] = {
-    'searchSourceJSON': '{"filter":[{"query":{"query_string":{"query":"*","analyze_wildcard":true}}}],'
-    '"highlightAll":true,"version":true}'
-}
-
-# Check if visualizations already present in dashboard. If present, don't add, else, add at end
-
-GET_URL = 'https://{}:{}/.kibana/dashboard/{}'.format(ELK_DB_HOST, ELK_DB_PORT, DASHBOARD_NAME)
-r = requests.get(GET_URL)
-response = r.json()
-dashboard_found = response['found']
-vis_ids_present = set()
-panelsJSON = []
-
-print(json.dumps(response, indent=4))
-if dashboard_found:
-    panelsJSON = yaml.safe_load(response['_source']['panelsJSON'])
-    for vis in panelsJSON:
-        vis_ids_present.add(vis['id'])
-
-size_x = 6
-size_y = 3
-xpos = (len(vis_ids_present) % 2) * 6 + 1
-ypos = (len(vis_ids_present) / 2) * 3 + 1
-for (i, vis_id) in enumerate(vis_ids):
-    if (not dashboard_found or vis_id not in vis_ids_present):
-        panelJSON = {
-            'size_x': size_x,
-            'size_y': size_y,
-            'panelIndex': len(vis_ids_present) + i,
-            'type': 'visualization',
-            'id': vis_id,
-            'col': xpos,
-            'row': ypos,
-        }
-        xpos += size_x
-        if xpos > 12:
-            xpos = 1
-            ypos += size_y
-        panelsJSON.append(panelJSON)
-    else:
-        print('visualization ' + vis_id + ' already present in dashboard')
-
-dashboard['panelsJSON'] = JSONToString(panelsJSON)
-PUT_URL = 'https://{}:{}/.kibana/dashboard/{}'.format(ELK_DB_HOST, ELK_DB_PORT, DASHBOARD_NAME)
-print(PUT_URL)
-print(json.dumps(dashboard, indent=4))
-r = requests.put(PUT_URL, json=dashboard)
-print(r.status_code)
-print(json.dumps(r.json(), indent=4))
