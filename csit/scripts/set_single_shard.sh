47b2f69d80e2f6f70f520090f5cb4f9966e24ad3
Faseela K <faseela.k@ericsson.com>
2018-06-13 14:24:12 +0530

    Set single shard for netvirt 3 node CSIT
    Remove distinct shard configuration for inventory and
    topology, and move them to default shard.
    
    JIRA: NETVIRT-1319
    
    Change-Id: Idd650964bae427b35c91fa10ce4e39bbe946fa7b
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ csit/scripts/set_single_shard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Add single shard config file"
+cat > ${WORKSPACE}/custom_shard_config.txt <<EOF
+FRIENDLY_MODULE_NAMES[1]='toaster'
+MODULE_NAMESPACES[1]='http://netconfcentral.org/ns/toaster'
+EOF
+
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+    CONTROLLERIP=ODL_SYSTEM_${i}_IP
+    echo "Copy shard config to member-${i} with IP address ${!CONTROLLERIP}"
+    scp ${WORKSPACE}/custom_shard_config.txt ${!CONTROLLERIP}:/tmp/
+done
