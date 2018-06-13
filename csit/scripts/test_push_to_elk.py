ba941c6b56a6a3e2beb62bf8b59389bf1100f376
Taseer Ahmed <taseer94@gmail.com>
2018-06-13 23:18:35 +0500

    Add minimal unit tests
    - Modularize push_to_elk script
    - Setup environment variables
    - Add dummy data under testfiles
    
    Change-Id: I49a3f61a6a9632f53f8819ef24fd12163036e170
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ csit/scripts/test_push_to_elk.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2018 Taseer Ahmed and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+from mock import patch
+import os
+import unittest
+
+
+class TestJsonPayLoad(unittest.TestCase):
+
+    """
+    Tests the JSON payload pushed to Elastic
+    """
+
+    def test_json_type(self):
+        with patch.dict('os.environ', {'SILO': 'jenkins',
+                                       'JOB_NAME': 'csit',
+                                       'BUILD_NUMBER': '5',
+                                       'WORKSPACE': os.path.join(os.path.dirname(__file__), 'testfiles')}):
+            import push_to_elk
+            payload = push_to_elk.construct_json()
+            assert(isinstance(payload, dict))
+
+
+if __name__ == '__main__':
+    unittest.main()
