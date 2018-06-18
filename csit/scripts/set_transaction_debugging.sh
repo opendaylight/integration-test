899de5d1c419f5d3e0a794b978db768167e6160a
Jamo Luhrsen <jluhrsen@redhat.com>
2018-06-18 10:08:09 -0700

    transaction-debug-context-enabled=true in HA jobs
    Change-Id: I13d8f0b7105e3772b16b669571eebeafcadf38c5
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

---
+++ csit/scripts/set_transaction_debugging.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+cat > ${WORKSPACE}/set_transaction_debugging.sh <<EOF
+
+    export CONFFILE=\`find /tmp/${BUNDLEFOLDER} -name "*cluster*datastore.cfg"\`
+    cp \$CONFFILE /tmp/${BUNDLEFOLDER}/etc/org.opendaylight.controller.cluster.datastore.cfg
+    echo "transaction-debug-context-enabled=true" >> /tmp/${BUNDLEFOLDER}/etc/org.opendaylight.controller.cluster.datastore.cfg
+    cat /tmp/${BUNDLEFOLDER}/etc/org.opendaylight.controller.cluster.datastore.cfg
+
+EOF
+
+echo "Copying config files to ODL Controller folder"
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
+
+        echo "Setting transaction-debug-context-enabled to true on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/set_transaction_debugging.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash /tmp/set_transaction_debugging.sh'
+
+done
