57e21c3ce212ade37d1b3ec93d05b6aa3d44660e
Karthika Panneer <karthikapaneer97@gmail.com>
2018-08-13 08:21:43 -0700

    Scalability of VMs in COE
    Change-Id: I63a067c258b32e39b3a1086b4c0c2ae5a3716da0
    Signed-off-by: Karthika Panneer <karthikapaneer97@gmail.com>

--- csit/scripts/set_unique_node_uuid.sh
+++ csit/scripts/set_unique_node_uuid.sh
@@ -10,3 +10,7 @@ scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_2_IP}:/tmp/
 ssh ${TOOLS_SYSTEM_2_IP} 'sudo bash /tmp/system-ovs-restart.sh'
 scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_3_IP}:/tmp/
 ssh ${TOOLS_SYSTEM_3_IP} 'sudo bash /tmp/system-ovs-restart.sh'
+scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_4_IP}:/tmp/
+ssh ${TOOLS_SYSTEM_2_IP} 'sudo bash /tmp/system-ovs-restart.sh'
+scp ${WORKSPACE}/system-ovs-restart.sh ${TOOLS_SYSTEM_5_IP}:/tmp/
+ssh ${TOOLS_SYSTEM_2_IP} 'sudo bash /tmp/system-ovs-restart.sh'
