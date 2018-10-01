60dda9c51323f0e751a75cbd55b916097bfc5d33
Sam Hague <shague@redhat.com>
2018-10-01 13:04:59 -0400

    Use bytes in place of unicode for python 3.4
    unicode is not defined in python 3.4 - it is in 2.7. bytes is defined
    in both, but in 2.7 it means the same thing as unicode so it can be
    used in both versions.
    
    Change-Id: Iefc109f28eed491aad9fac7bd9093685cba2348d
    Signed-off-by: Sam Hague <shague@redhat.com>

--- tools/distchanges/gerritquery.py
+++ tools/distchanges/gerritquery.py
@@ -74,12 +74,15 @@ class GerritQuery:
 
     @staticmethod
     def print_safe_encoding(string):
-        if type(string) == unicode:
-            encoding = 'utf-8'
-            if hasattr(sys.stdout, 'encoding') and sys.stdout.encoding:
-                encoding = sys.stdout.encoding
-            return string.encode(encoding or 'utf-8', 'replace')
-        else:
+        try:
+            if type(string) == unicode:
+                encoding = 'utf-8'
+                if hasattr(sys.stdout, 'encoding') and sys.stdout.encoding:
+                    encoding = sys.stdout.encoding
+                return string.encode(encoding or 'utf-8', 'replace')
+            else:
+                return str(string)
+        except:
             return str(string)
 
     def run_command_status(self, *argv, **kwargs):
@@ -281,13 +284,19 @@ class GerritQuery:
         :return list: Lines of the JSON
         """
         lines = []
-        for line in changes.split("\n"):
+        skipped = 0
+        for i, line in enumerate(changes.split("\n")):
+            logger.info("line {}: {}".format(i, line))
             if line.find('"type":"error","message"') != -1:
                 logger.warn("there was a query error")
+                skipped += 1
                 continue
-            if line.find('stats') == -1:
+            if line.find('"type":"stats"') == -1:
                 lines.append(line)
-        logger.debug("get_gerrit_lines: found %d lines", len(lines))
+            else:
+                logging.debug("skipping: {}".format(line))
+                skipped += 1
+        logger.debug("get_gerrit_lines: found {} lines, skipped: {}".format(len(lines), skipped))
         return lines
 
     def get_gerrits(self, project, changeid=None, limit=1, msg=None, status=None, comments=False, commitid=None):
@@ -326,6 +335,6 @@ class GerritQuery:
             return gerrits
         try:
             sorted_gerrits = sorted(gerrits, key=itemgetter('grantedOn'), reverse=True)
-        except KeyError, e:
+        except KeyError as e:
             logger.warn("KeyError exception in %s, %s", project, str(e))
         return sorted_gerrits
