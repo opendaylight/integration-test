91acb5252dac2f7ca2133f4805e0147e38e78908
Ivan Hrasko <ihrasko@cisco.com>
2018-10-04 12:34:43 +0200

    SXP: download SXP bundle on tools node
    - SXP already installs ODL on tools node,
    but managed distribution only
    - This patch downloads actual SXP karaf
    distribution, installs basic SXP features
    and run it
    
    Change-Id: Ie32c49713e6fe0fc1079622284a98833fd42ed14
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- csit/suites/sxp/scripts/tools_node_setup.sh
+++ csit/suites/sxp/scripts/tools_node_setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 TOOLS_WORK_DIR="/tmp"
 
-echo "Extracting the new controller... [${TOOLS_SYSTEM_IP}]"
-ssh ${TOOLS_SYSTEM_IP} wget --progress=dot:mega ${ACTUAL_BUNDLE_URL} -P ${TOOLS_WORK_DIR}
-ssh ${TOOLS_SYSTEM_IP} unzip -q ${TOOLS_WORK_DIR}/${BUNDLE} -d ${TOOLS_WORK_DIR}
+# Get SXP version for current ${GERRIT_BRANCH}
+wget "https://git.opendaylight.org/gerrit/gitweb?p=sxp.git;a=blob_plain;f=sxp-core/pom.xml;hb=refs/heads/${GERRIT_BRANCH}" -O "pom.xml"
+SXP_VERSION=`xmllint --xpath '/*[local-name()="project"]/*[local-name()="parent"]/*[local-name()="version"]/text()' pom.xml`
+
+# Get the latest sxp-karaf build URL for ${SXP_VERSION}
+NEXUS_URL=https://nexus.opendaylight.org/content/repositories/opendaylight.snapshot/org/opendaylight/sxp/sxp-karaf/${SXP_VERSION}
+wget ${NEXUS_URL}/maven-metadata.xml -O maven-metadata.xml
+REVISION=`awk -vRS="</value>" '{gsub(/.*<value.*>/,"");print}' maven-metadata.xml | sed -n 1p`
+SXP_BUNDLE_URL=${NEXUS_URL}/sxp-karaf-${REVISION}.zip
+
+echo "Extracting the new controller [${TOOLS_SYSTEM_IP}] with ODL: [${SXP_BUNDLE_URL}]"
+ssh ${TOOLS_SYSTEM_IP}
+ssh ${TOOLS_SYSTEM_IP} wget --progress=dot:mega ${SXP_BUNDLE_URL} -P ${TOOLS_WORK_DIR}
+ssh ${TOOLS_SYSTEM_IP} unzip -q ${TOOLS_WORK_DIR}/sxp-karaf-${REVISION}.zip -d ${TOOLS_WORK_DIR}
 
 echo "Set Java version"
 if [ ${JDKVERSION} == 'openjdk8' ]; then
@@ -20,11 +31,11 @@ if [ ${JDKVERSION} == 'openjdk8' ]; then
 fi
 
 echo "Configuring the startup features..."
-TOOLS_FEATURESCONF=${TOOLS_WORK_DIR}/${BUNDLEFOLDER}/etc/org.apache.karaf.features.cfg
-ssh ${TOOLS_SYSTEM_IP} "sed -r -i.old \"s/featuresBoot ?=.*/featuresBoot=config,standard,region,package,kar,ssh,management,${ACTUALFEATURES}/g\" ${TOOLS_FEATURESCONF}"
+TOOLS_FEATURESCONF=${TOOLS_WORK_DIR}/sxp-karaf-${SXP_VERSION}/etc/org.apache.karaf.features.cfg
+ssh ${TOOLS_SYSTEM_IP} "sed -r -i.old \"s/featuresBoot ?=.*/featuresBoot=config,standard,region,package,kar,ssh,management,${ACTUALFEATURES},odl-sxp-core,odl-sxp-controller/g\" ${TOOLS_FEATURESCONF}"
 
 echo "Starting controller..."
-ssh ${TOOLS_SYSTEM_IP} "${TOOLS_WORK_DIR}/${BUNDLEFOLDER}/bin/start"
+ssh ${TOOLS_SYSTEM_IP} "${TOOLS_WORK_DIR}/sxp-karaf-${SXP_VERSION}/bin/start"
 
 echo "Waiting for controller to come up..."
 COUNT="0"
