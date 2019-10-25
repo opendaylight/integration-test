ac0916a1642dafd5bd33a3036ae24c4021898b23
Faseela K <k.faseela@gmail.com>
2019-10-25 11:21:25 +0530

    Remove COE Netvirt CSIT Scripts
    Signed-off-by: Faseela K <k.faseela@gmail.com>
    Change-Id: I5b045ff16acdfb2edcd9ce871fddc08cb2a9ab5f

--- csit/variables/coe/Modules.py
+++
@@ -1,7 +0,0 @@
-coe_data_models = [
-    'config/k8s-cluster:k8s-clusters-info',
-    'config/k8s-node:k8s-nodes-info',
-    'config/pod:coe',
-    'config/service:service-information',
-    'operational/pod-meta:podidentifier-info',
-]
