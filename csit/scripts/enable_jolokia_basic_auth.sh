30114b903fc3b0ba5f102744705c412c5ae1b826
Jamo Luhrsen <jluhrsen@gmail.com>
2020-01-22 14:10:22 -0800

    Add new restconf auth suite
    Change-Id: I4136c93f43489c4c720cf42b62707246355cae89
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

---
+++ csit/scripts/enable_jolokia_basic_auth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cat > ${WORKSPACE}/enable_jolokia_basic_auth.sh <<EOF
+
+    export CONFFILE=\`find /tmp/${BUNDLEFOLDER} -name "*org.jolokia.osgi.cfg"\`
+    cp \$CONFFILE /tmp/${BUNDLEFOLDER}/etc/org.jolokia.osgi.cfg
+    echo "org.jolokia.authMode=basic" >> /tmp/${BUNDLEFOLDER}/etc/org.jolokia.osgi.cfg
+    echo "org.jolokia.user=admin" >> /tmp/${BUNDLEFOLDER}/etc/org.jolokia.osgi.cfg
+    echo "org.jolokia.password=admin" >> /tmp/${BUNDLEFOLDER}/etc/org.jolokia.osgi.cfg
+    cat /tmp/${BUNDLEFOLDER}/etc/org.jolokia.osgi.cfg
+
+EOF
+
+echo "Copying config files to ODL Controller folder"
+for i in `seq 1 ${NUM_ODL_SYSTEM}`
+do
+        CONTROLLERIP=ODL_SYSTEM_${i}_IP
+
+        echo "Enabling jolokia basic auth with default values on ${!CONTROLLERIP}"
+        scp ${WORKSPACE}/enable_jolokia_basic_auth.sh ${!CONTROLLERIP}:/tmp/
+        ssh ${!CONTROLLERIP} 'bash /tmp/enable_jolokia_basic_auth.sh'
+
+done
