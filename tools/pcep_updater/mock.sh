86ba39ba31f1f226b91d7dd3588b446c30ef8161
BhuvanaVignesh <bvignesh@luminanetworks.com>
2020-07-02 16:24:06 +0530

    BGPCEP-901 Tests for BGPCEP-901
    Signed-off-by: BhuvanaVignesh <bvignesh@luminanetworks.com>
    Change-Id: Ifb73a165244ddb7fbbd77327479edf0331af774f

---
+++ tools/pcep_updater/mock.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## Permform some validation on input arguments.
+if [ -z "$7" ]; then
+        echo "Missing arguments, exiting.."
+        exit 1
+fi
+
+for i in {1..3}
+do
+    echo       "----------------------------------------------------------"
+    java -jar ~/$1 --local-address $2 --remote-address $3 --pcc $4 --lsp $5 &>> /tmp/throughpcep_$6.log
+
+    process_id=`/bin/ps -fu $USER| grep "$HOME/$1" | grep -v "grep" | awk '{print $2}'`
+    echo "PCC MOCK Process id for $a iteration is $process_id"
+    sleep $7m
+
+    ps -ef |grep "$HOME/$1"  | grep -v "grep"| awk '{print $2}' | xargs kill
+
+done
