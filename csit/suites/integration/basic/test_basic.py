4cd539cadb6d88037fd3db8ad90ba62d98f99062
Satabrata Paul <paulsatabrata@gmail.com>
2022-06-17 09:29:41 +0530

    Update basic integration test
    Signed-off-by: Satabrata Paul <paulsatabrata@gmail.com>
    Change-Id: Ibeb9c6b2073f520e91d6e7c5ea05af66c42e1b70

--- csit/suites/integration/basic/test_basic.py
+++ csit/suites/integration/basic/test_basic.py
@@ -1,36 +1,29 @@
-#Basic test to get the restconf modules, check 200 status and ietf-restconf presence.
-#Importing required packages
+# Basic test to get the restconf modules, check 200 status and ietf-restconf presence.
+# Importing required packages
 import requests
 import logging
 
-ODL_SYSTEM_1_IP = "127.0.0.1"
-ODL_SYSTEM_2_IP = "127.0.0.2"
-ODL_SYSTEM_1_IP = "127.0.0.3"
-ODL_SYSTEM_IP = ODL_SYSTEM_1_IP
-RESTCONFPORT = 8181
-ODL_RESTCONF_USER = "admin"
-ODL_RESTCONF_PASSWORD = "admin"
-AUTH = (ODL_RESTCONF_USER,ODL_RESTCONF_PASSWORD)
-HEADERS_XML = {'Content-Type': 'application/xml'}
-MODULES_API = "/rests/data/ietf-yang-library:modules-state"
+# Function to create a session
 
-#Function to create a session
-def create_session(AUTH, HEADER):
-    #Creating sesion object
+
+def create_session(auth, header):
+    # Creating sesion object
     session = requests.Session()
-    #Authentication
-    session.auth = AUTH
-    #Updating headers
-    session.headers.update(HEADER)
+    # Authentication
+    session.auth = auth
+    # Updating headers
+    session.headers.update(header)
     return session
 
-#Function to make a get request
-def make_get_request():
-    session = create_session(AUTH, HEADERS_XML)
+# Function to make a get request
 
-    #Making get request
+
+def make_get_request(auth, headers, odl_system_ip, restconfport, modules_api):
+    session = create_session(auth, headers)
+
+    # Making get request
     try:
-        resp = session.get("http://{}:{}/{}".format(ODL_SYSTEM_IP, RESTCONFPORT, MODULES_API))
+        resp = session.get("http://{}:{}/{}".format(odl_system_ip, restconfport, modules_api))
         logging.info(self.resp.json())
         return resp
     except Exception(e):
@@ -39,8 +32,9 @@ def make_get_request():
 
 class TestBasic:
 
-    #Make api call
-    resp = make_get_request()
+    # Make api call
+    resp = make_get_request(('admin', 'admin'), {'Content-Type': 'application/xml'},
+                            '127.0.0.1', 8181, '/rests/data/ietf-yang-library:modules-state')
 
     def test_get_restconf_module(self):
         assert isinstance(self.resp, requests.models.Response) == True
@@ -49,7 +43,8 @@ class TestBasic:
         assert self.resp.status_code == 200
 
     def test_content(self):
-        assert self.resp.content == "ietf-restconf"
-        #Closing the session
-        #Placing this line here, because this test will be run last in sequential order by pytest
-        self.resp.close() 
+        # Checking if the text content contains "ietf-restconf" or not
+        assert self.resp.text.find("ietf-restconf") != -1
+        # Closing the session
+        # Placing this line here, because this test will be run last in sequential order by pytest
+        self.resp.close()
