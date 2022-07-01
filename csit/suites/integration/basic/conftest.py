f62526282cc0a5e6fa9fb16b047dca688d49683e
SatabrataPaul <paulsatabrata@gmail.com>
2022-07-01 16:53:16 +0530

    Update directories to include __init__.py for relative package imports
    Signed-off-by: SatabrataPaul <paulsatabrata@gmail.com>
    Change-Id: I48c6a05bbca88b1a410c36d48ccc630116525b64

---
+++ csit/suites/integration/basic/conftest.py
@@ -0,0 +1,7 @@
+def pytest_itemcollected(item):
+    par = item.parent.obj
+    node = item.obj
+    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
+    suf = node.__doc__.strip() if node.__doc__ else node.__name__
+    if pref or suf:
+        item._nodeid = " ".join((pref, suf))
