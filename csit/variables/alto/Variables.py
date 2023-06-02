ac001287a8a4f8f0fcd5e295d0db0f78a0b0fa21
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-06-02 14:04:08 +0300

    Adapt python scripts to RFC 8040 (variables)
    Python scripts inside "variables" were adapted.
    
    JIRA: INTTEST-138
    Change-Id: I2de179e808511677b49efa24a8ae5cc0f6c14563
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- csit/variables/alto/Variables.py
+++ csit/variables/alto/Variables.py
@@ -1,9 +1,9 @@
-RESOURCE_POOL_BASE = "restconf/operational/alto-resourcepool:context"
+RESOURCE_POOL_BASE = "rests/data/alto-resourcepool:context"
 DEFAULT_CONTEXT_ID = "00000000-0000-0000-0000-000000000000"
 
 # ALTO IRD variables
-ALTO_SIMPLE_IRD_INFO = "restconf/operational/alto-simple-ird:information"
+ALTO_SIMPLE_IRD_INFO = "rests/data/alto-simple-ird:information"
 ALTO_CONFIG_IRD_INSTANCE_CONFIG = (
-    "restconf/config/alto-simple-ird:ird-instance-configuration"
+    "rests/data/alto-simple-ird:ird-instance-configuration"
 )
-ALTO_OPERATIONAL_IRD_INSTANCE = "restconf/operational/alto-simple-ird:ird-instance"
+ALTO_OPERATIONAL_IRD_INSTANCE = "rests/data/alto-simple-ird:ird-instance"
