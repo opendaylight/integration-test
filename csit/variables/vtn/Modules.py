45a6f3b8f5248c200d30a8519bdd41d34375b990
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-08-13 13:39:25 +0200

    Remove Vtn project code
    Remove all code related to the Vtn project as part of the cleanup
    process for projects no longer included in the distribution.
    
    JIRA: INTTEST-144
    Change-Id: I9f626e991a7dcf26163358954bf797b058af0ff2
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- csit/variables/vtn/Modules.py
+++
@@ -1,8 +0,0 @@
-vtn_data_models = [
-    "config/network-topology:network-topology/topology/ovsdb:1",
-    "config/opendaylight-inventory:nodes",
-    "operational/opendaylight-inventory:nodes",
-    "operational/entity-owners:entity-owners",
-    "operational/vtn-inventory:vtn-nodes",
-    "operational/network-topology:network-topology/topology/ovsdb:1",
-]
