0813ca26c05d309055319255fc28d063d0507fcc
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-09-18 16:53:23 +0200

    Adapt CSIT for netconf-node state encapsulation
    Adapt scripts used in scale test suites.
    
    JIRA: INTTEST-146
    Change-Id: I4ebcc33b1c00d40bc74023f2f936406c72bb6b8b
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- tools/netconf_tools/configurer.py
+++ tools/netconf_tools/configurer.py
@@ -122,19 +122,44 @@ def parse_arguments():
         type=str2bool,
         help="Should single requests session be re-used",
     )
+    parser.add_argument(
+        "--version",
+        default="scandium",
+        help="Version of payload data to be used",
+    )
     return parser.parse_args()  # arguments are read
 
 
-DATA_TEMPLATE = string.Template(
+CALCIUM_DATA_TEMPLATE = string.Template(
     """{
     "network-topology:node": {
         "node-id": "$DEVICE_NAME",
         "netconf-node-topology:host": "$DEVICE_IP",
         "netconf-node-topology:port": $DEVICE_PORT,
-        "netconf-node-topology:username": "$DEVICE_USER",
-        "netconf-node-topology:password": "$DEVICE_PASSWORD",
+        "netconf-node-topology:login-password-unencrypted": {
+            "netconf-node-topology:username": "$DEVICE_USER",
+            "netconf-node-topology:password": "$DEVICE_PASSWORD",
+        },
         "netconf-node-topology:tcp-only": "false",
         "netconf-node-topology:keepalive-delay": 0
+    }
+}"""
+)
+
+SCANDIUM_DATA_TEMPLATE = string.Template(
+    """{
+    "network-topology:node": {
+        "node-id": "$DEVICE_NAME",
+        "netconf-node":{
+            "netconf-node-topology:host": "$DEVICE_IP",
+            "netconf-node-topology:port": $DEVICE_PORT,
+            "netconf-node-topology:login-password-unencrypted": {
+                "netconf-node-topology:username": "$DEVICE_USER",
+                "netconf-node-topology:password": "$DEVICE_PASSWORD",
+            },
+            "netconf-node-topology:tcp-only": "false",
+            "netconf-node-topology:keepalive-delay": 0
+        }
     }
 }"""
 )
@@ -211,7 +236,10 @@ def main():
             put_name = args.basename + "-" + str(port) + "-" + str(iteration)
             subst_dict["DEVICE_NAME"] = put_name
             subst_dict["DEVICE_PORT"] = str(port)
-            put_data = DATA_TEMPLATE.substitute(subst_dict)
+            if args.version == "calcium":
+                put_data = CALCIUM_DATA_TEMPLATE.substitute(subst_dict)
+            else:
+                put_data = SCANDIUM_DATA_TEMPLATE.substitute(subst_dict)
             uri = uri_part + put_name
             response = AuthStandalone.Put_Using_Session(
                 session, uri, data=put_data, headers=put_headers
